d176aa61b7140a358c41e37d81d4e20e2ea0d8dc
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-01-29 12:20:48 +0330

    fix bug

--- ledger/views/__init__.py
+++ ledger/views/__init__.py
@@ -2,5 +2,5 @@ from .asset_info_view import AssetsView
 from .wallet_view import WalletView
 from .deposit_address_view import DepositAddressView
 from .otc_trade_view import OTCTradeRequestView, OTCTradeView
-from .margin_view import MarginInfoView, MarginTransferView
+from .margin_view import MarginInfoView, MarginTransferView, MarginLoanView
 from .withdraw_view import WithdrawView
