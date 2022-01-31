c18c5791d7e4a5330b69941b5f1442d5eb7cc491
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-01-31 10:10:23 +0330

    fix bug

--- ledger/models/asset.py
+++ ledger/models/asset.py
@@ -21,8 +21,8 @@ class Asset(models.Model):
     IRT = 'IRT'
     USDT = 'USDT'
 
-    live_objects = LiveAssetManager()
     objects = models.Manager()
+    live_objects = LiveAssetManager()
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
