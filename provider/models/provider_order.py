9bab3139f8fc6bb2ad2753bfe5b329cbd70717eb
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-02-05 15:15:56 +0330

    fix bug

--- provider/models/provider_order.py
+++ provider/models/provider_order.py
@@ -53,7 +53,7 @@ class ProviderOrder(models.Model):
             symbol = cls.get_trading_symbol(asset)
 
             if asset.symbol == 'SHIB':
-                symbol.replace('SHIB', '1000SHIB')
+                symbol = symbol.replace('SHIB', '1000SHIB')
                 amount = round(amount / 1000)
 
             resp = BinanceFuturesHandler.place_order(
