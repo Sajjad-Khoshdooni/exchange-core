d15ddefb938078e4a54a2eb672f482061179e043
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-02-06 17:55:48 +0330

    fix admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -71,6 +71,8 @@ class NetworkAdmin(admin.ModelAdmin):
     list_display = ('symbol', 'can_withdraw', 'can_deposit', 'min_confirm', 'unlock_confirm', 'address_regex')
     list_editable = ('can_withdraw', 'can_deposit')
     search_fields = ('symbol', )
+    list_filter = ('can_withdraw', 'can_deposit')
+    ordering = ('-can_withdraw', '-can_deposit')
 
 
 @admin.register(models.NetworkAsset)
