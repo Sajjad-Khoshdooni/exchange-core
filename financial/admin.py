e650dbef319f955e8b5d005eb70d28121d6fe046
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-02-13 15:02:01 +0100

    add admin

--- financial/admin.py
+++ financial/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from financial.models import Gateway, PaymentRequest, Payment
+
+
+@admin.register(Gateway)
+class GatewayAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'merchant_id', 'active')
+    list_editable = ('active', )
+
+
+@admin.register(PaymentRequest)
+class PaymentRequestAdmin(admin.ModelAdmin):
+    list_display = ('created', 'gateway', 'bank_card', 'amount', 'authority')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('created', 'status', 'ref_id', 'ref_status', )
