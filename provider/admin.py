9d8fb2fa891cea1678aca6a425a7e0a953669ac7
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-02-16 17:54:03 +0100

    add admin

--- provider/admin.py
+++ provider/admin.py
@@ -4,5 +4,15 @@ from provider import models
 
 
 @admin.register(models.ProviderOrder)
-class ProviderAdmin(admin.ModelAdmin):
+class ProviderOrderAdmin(admin.ModelAdmin):
     list_display = ('created', 'asset', 'side', 'amount', 'order_id')
+
+
+@admin.register(models.ProviderTransfer)
+class ProviderTransferAdmin(admin.ModelAdmin):
+    list_display = ('created', 'asset', 'network', 'amount', 'address', 'provider_transfer_id', 'caller_id')
+
+
+@admin.register(models.ProviderHedgedOrder)
+class ProviderHedgedOrderAdmin(admin.ModelAdmin):
+    list_display = ('created', 'asset', 'side', 'amount', 'caller_id', 'hedged')
