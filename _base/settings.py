f6761741100d641f46095f49227d22a3258bf1dc
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-02-16 10:06:13 +0100

    add IGNORABLE_404_URLS

--- _base/settings.py
+++ _base/settings.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,6 +153,11 @@ MEDIA_ROOT = config('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media/'))
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+IGNORABLE_404_URLS = (
+    re.compile('/favicon.ico'),
+)
 
 SENTRY_CLIENT = 'raven.contrib.django.raven_compat.DjangoClient'
 RAVEN_CONFIG = {
