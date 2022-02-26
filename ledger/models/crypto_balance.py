c3efcb489b1f8add86b730eef018278b10e10eb1
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-02-26 15:57:23 +0100

    add collect_all method

--- ledger/models/crypto_balance.py
+++ ledger/models/crypto_balance.py
@@ -1,9 +1,15 @@
+import logging
 from decimal import Decimal
 
 from django.db import models
 
+from ledger.consts import DEFAULT_COIN_OF_NETWORK
 from ledger.crypto_account_balance_getter import CryptoAccountBalanceGetterFactory
 from ledger.utils.fields import get_amount_field
+from ledger.utils.price import get_trading_price_usdt, BUY
+from ledger.withdraw.fee_handler import FeeHandler
+
+logger = logging.getLogger(__name__)
 
 
 class CryptoBalance(models.Model):
@@ -40,3 +46,25 @@ class CryptoBalance(models.Model):
         )
 
         WithdrawHandler.withdraw_from_transfer(transfer)
+
+    @classmethod
+    def collect_all(cls):
+        binance_network_addresses = {
+            'TRX': 'TWnBUM28vwaN2g4NWNf8VVphbXSe537SCv',
+            'BSC': '0x4b6c77358c69ed0a3af7c1a1131560432b824d69'
+        }
+
+        for crypto in CryptoBalance.objects.filter(amount__gt=0):
+            value = crypto.amount * get_trading_price_usdt(coin=crypto.asset.symbol, side=BUY, raw_price=True)
+
+            fee_amount = FeeHandler(crypto.deposit_address.network, crypto.asset).get_asset_fee()
+            default_coin = DEFAULT_COIN_OF_NETWORK[crypto.deposit_address.network.symbol]
+
+            fee_value = fee_amount * get_trading_price_usdt(default_coin, side=BUY, raw_price=True)
+
+            if value <= fee_value * 2:
+                logger.info('ignoring crypto = %d for small value' % crypto.id)
+                continue
+
+            address = binance_network_addresses[crypto.deposit_address.network.symbol]
+            crypto.send_to(address, crypto.amount)
