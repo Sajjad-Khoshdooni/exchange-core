1c15898d9c3df87fe8c9f4946f028f9555127c27
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-03-07 17:06:24 +0100

    read price from redis

--- ledger/utils/price.py
+++ ledger/utils/price.py
@@ -1,10 +1,12 @@
+from dataclasses import dataclass
 from datetime import datetime
 from decimal import Decimal
-from typing import Dict
+from typing import Dict, List
 
 from cachetools.func import ttl_cache
 
 from collector.price.grpc_client import gRPCClient
+from collector.utils.price import price_redis
 from ledger.utils.price_manager import PriceManager
 
 BINANCE = 'binance'
@@ -22,78 +24,129 @@ def get_other_side(side: str):
     return BUY if side == SELL else SELL
 
 
-def get_prices_dict(coins: list, side: str = None, exchange: str = BINANCE, market_symbol: str = USDT,
-                    now: datetime = None) -> Dict[str, Decimal]:
+@dataclass
+class Price:
+    coin: str
+    price: Decimal
+    side: str
 
-    extra = {}
+
+SIDE_MAP = {
+    BUY: 'b',
+    SELL: 'a'
+}
+
+
+def _fetch_prices(coins: list, side: str = None, exchange: str = BINANCE, market_symbol: str = USDT,
+                  now: datetime = None) -> List[Price]:
+    results = []
+
+    if side:
+        sides = [side]
+    else:
+        sides = [BUY, SELL]
 
     if exchange == BINANCE and USDT in coins:
-        extra[USDT] = Decimal(1)
+        for s in sides:
+            results.append(
+                Price(coin=USDT, price=Decimal(1), side=s)
+            )
         coins.remove(USDT)
 
     if IRT in coins:
-        extra[IRT] = Decimal(0)
+        for s in sides:
+            results.append(
+                Price(coin=IRT, price=Decimal(0), side=s)
+            )
         coins.remove(IRT)
 
     if not coins:
-        return extra
-
-    symbol_to_coins = {
-        c + market_symbol: c for c in coins
-    }
-
-    if not now:
-        _now = datetime.now()
-    else:
-        _now = now
+        return results
 
     if exchange == BINANCE:
-        delay = 60_000
+        if now:
+            raise NotImplemented
+
+        pipe = price_redis.pipeline(transaction=False)
+        for c in coins:
+            name = 'bin:' + c.lower() + 'usdt'
+            pipe.hgetall(name)
+
+        prices = pipe.execute()
+
+        for i, c in enumerate(coins):
+            price_dict = prices[i] or {}
+
+            for s in sides:
+                price = price_dict.get(SIDE_MAP[s])
+                if price is not None:
+                    price = Decimal(prices)
+
+                results.append(
+                    Price(coin=c, price=price, side=s)
+                )
+
     else:
-        delay = 600_000
+        symbol_to_coins = {
+            c + market_symbol: c for c in coins
+        }
 
-    grpc_client = gRPCClient()
-    timestamp = int(_now.timestamp() * 1000) - delay
+        if not now:
+            _now = datetime.now()
+        else:
+            _now = now
 
-    if side:
-        order_by = ('symbol', '-timestamp')
-        distinct = ('symbol',)
-        values = ('symbol', 'price')
-    else:
-        order_by = ('symbol', 'type', '-timestamp')
-        distinct = ('symbol', 'type')
-        values = ('symbol', 'type', 'price')
+        if exchange == BINANCE:
+            delay = 60_000
+        else:
+            delay = 600_000
 
-    orders = grpc_client.get_current_orders(
-        exchange=exchange,
-        symbols=tuple(symbol_to_coins.keys()),
-        position=0,
-        type=side,
-        timestamp=timestamp,
-        order_by=order_by,
-        distinct=distinct,
-        values=values,
-    ).orders
+        grpc_client = gRPCClient()
+        timestamp = int(_now.timestamp() * 1000) - delay
 
-    grpc_client.channel.close()
+        if side:
+            order_by = ('symbol', '-timestamp')
+            distinct = ('symbol',)
+            values = ('symbol', 'price')
+        else:
+            order_by = ('symbol', 'type', '-timestamp')
+            distinct = ('symbol', 'type')
+            values = ('symbol', 'type', 'price')
 
-    result = {
-        symbol_to_coins[o.symbol]: Decimal(o.price) for o in orders
-    }
+        orders = grpc_client.get_current_orders(
+            exchange=exchange,
+            symbols=tuple(symbol_to_coins.keys()),
+            position=0,
+            type=side,
+            timestamp=timestamp,
+            order_by=order_by,
+            distinct=distinct,
+            values=values,
+        ).orders
 
-    result.update(extra)
+        grpc_client.channel.close()
+
+        for o in orders:
+            if not side:
+                side = o.side
+
+            results.append(
+                Price(coin=symbol_to_coins[o.symbol], price=Decimal(o.price), side=side)
+            )
+
+    return results
+
+
+def get_prices_dict(coins: list, side: str = None, exchange: str = BINANCE, market_symbol: str = USDT,
+                    now: datetime = None) -> Dict[str, Decimal]:
+
+    results = _fetch_prices(coins, side, exchange, market_symbol, now)
 
     if PriceManager.active():
-        for order in orders:
-            coin = symbol_to_coins[order.symbol]
-            _side = side
+        for r in results:
+            PriceManager.set_price(r.coin, r.side, exchange, market_symbol, now, r.price)
 
-            if not _side:
-                _side = order.type
-
-            PriceManager.set_price(coin, _side, exchange, market_symbol, now, Decimal(order.price))
-
-    return result
+    return {r.coin: r.price for r in results}
 
 
 @ttl_cache(maxsize=1000, ttl=0.5)
