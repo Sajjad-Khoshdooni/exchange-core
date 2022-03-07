199041ae7b857d9301e9e8b44c705bef911ae04d
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-03-07 15:04:50 +0100

    add bulk redis set

--- collector/binance_consumer.py
+++ collector/binance_consumer.py
@@ -2,7 +2,9 @@ import json
 import logging
 import signal
 import sys
+import time
 from datetime import datetime
+
 import websocket
 
 from collector.utils.price import price_redis
@@ -15,9 +17,12 @@ BINANCE_WSS_URL = 'wss://stream.binance.com:9443/stream?streams='
 
 
 class BinanceConsumer:
-    def __init__(self):
+    def __init__(self, verbose: int = 0):
         self.loop = True
         self.socket = websocket.WebSocket()
+        self.queue = {}
+        self.last_flush_time = time.time()
+        self.verbose = verbose
 
         logger.info('Starting Binance Socket...')
 
@@ -38,14 +43,19 @@ class BinanceConsumer:
         self.socket.send(json.dumps({"method": "SUBSCRIBE", "params": streams,"id": 1}))
 
         while self.loop:
-            logger.info('Now %s' % datetime.now())
+            if self.verbose > 0:
+                logger.info('Now %s' % datetime.now())
+
             data_str = self.socket.recv()
             data = json.loads(data_str)
             self.handle_stream_data(data)
 
     def handle_stream_data(self, data: dict):
         if 'stream' not in data:
-            logger.info('ignoring %s' % data)
+
+            if self.verbose > 0:
+                logger.info('ignoring %s' % data)
+
             return
 
         stream = data['stream']
@@ -54,14 +64,30 @@ class BinanceConsumer:
         bid = data['data']['bids'][0][0]
         ask = data['data']['asks'][0][0]
 
-        logger.info('setting %s ask=%s, bid=%s' % (symbol, ask, bid))
+        if self.verbose > 0:
+            logger.info('setting %s ask=%s, bid=%s' % (symbol, ask, bid))
 
         key = 'bin:' + symbol
 
-        price_redis.hset(name=key, mapping={
+        self.queue[key] = {
             'a': ask, 'b': bid
-        })
-        price_redis.expire(key, 10)
+        }
+
+        if time.time() - self.last_flush_time > 1:
+            self.flush()
+
+    def flush(self):
+        logger.info('flushing %d items' % len(self.queue))
+        pipe = price_redis.pipeline(transaction=False)
+
+        for (name, data) in self.queue.items():
+            pipe.hset(name=name, mapping=data)
+            pipe.expire(name, 10)
+
+        pipe.execute()
+
+        self.queue = {}
+        self.last_flush_time = time.time()
 
     def exit_gracefully(self, signum, frame):
         self.loop = False
