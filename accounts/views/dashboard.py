79c710fb2a0e26ceb51542389bc0a8501d3dbb78
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-03-09 17:55:25 +0100

    add authentication

--- accounts/views/dashboard.py
+++ accounts/views/dashboard.py
@@ -1,15 +1,20 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.db.models import Q
 from django.shortcuts import render
+
 from accounts.models.user import User
 from financial.models.withdraw_request import FiatWithdrawRequest
+
+
+@staff_member_required
 def dashboard(request):
     if request.method == 'GET':
-        user = User.objects.all()
-        user_count = user.count()
-        user_pendin_or_reject_level_2_verification_count = user.filter(
+        users = User.objects.all()
+        user_count = users.count()
+        user_pendin_or_reject_level_2_verification_count = users.filter(
             (Q(verify_status='pending') | Q(verify_status='rejected'))
         ).filter(level=1).count()
-        user_pendin_or_reject_level_3_verification_count = user.filter(
+        user_pendin_or_reject_level_3_verification_count = users.filter(
             (Q(verify_status='pending') | Q(verify_status='reject'))
         ).filter(level=2).count()
         withdraw_request_pendeng_or_reject_count = FiatWithdrawRequest.objects.filter(
