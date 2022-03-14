30469ba5c0c86c30993a7c6a084f55bb207aafe6
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-03-14 14:28:53 +0100

    fix metrix labels

--- collector/metrics/defs.py
+++ collector/metrics/defs.py
@@ -5,15 +5,15 @@ from yekta_metrics import PrometheusClient
 prom = PrometheusClient()
 
 METRICS = {
-    'binance_total_hedge': prom.gauge('binance_total_hedge', 'binance total hedge'),
-    'binance_margin_balance': prom.gauge('binance_margin_balance', 'binance margin balance'),
-    'binance_initial_margin': prom.gauge('binance_initial_margin', 'binance initial margin'),
-    'binance_maintenance_margin': prom.gauge('binance_maintenance_margin', 'binance maintenance margin'),
+    'binance_total_hedge': prom.gauge('binance_total_hedge', 'binance total hedge', ['currency']),
+    'binance_margin_balance': prom.gauge('binance_margin_balance', 'binance margin balance', ['currency']),
+    'binance_initial_margin': prom.gauge('binance_initial_margin', 'binance initial margin', ['currency']),
+    'binance_maintenance_margin': prom.gauge('binance_maintenance_margin', 'binance maintenance margin', ['currency']),
     'binance_spot_tether': prom.gauge('binance_spot_tether', 'binance spot tether'),
-    'binance_future_margin_ratio': prom.gauge('binance_future_margin_ratio', 'binance future margin ratio'),
+    'binance_future_margin_ratio': prom.gauge('binance_future_margin_ratio', 'binance future margin ratio', ['currency']),
 
-    'binance_spot_value': prom.gauge('binance_spot_value', 'binance spot value'),
-    'internal_value': prom.gauge('internal_value', 'internal value'),
+    'binance_spot_value': prom.gauge('binance_spot_value', 'binance spot value', ['currency']),
+    'internal_value': prom.gauge('internal_value', 'internal value', ['currency']),
 
     'binance_price_updates': prom.counter('binance_price_updates', 'binance price updates'),
 }
