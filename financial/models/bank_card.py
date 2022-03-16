f849e262374204bf634f59cd149b7fdb0e00d6fe
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-03-16 10:09:37 +0100

    add ordering

--- financial/models/bank_card.py
+++ financial/models/bank_card.py
@@ -30,6 +30,7 @@ class BankCard(models.Model):
         verbose_name = 'کارت بانکی'
         verbose_name_plural = 'کارت‌های بانکی'
 
+        ordering = ('-verified', )
         unique_together = [('card_pan', 'user')]
 
         constraints = [
@@ -78,6 +79,7 @@ class BankAccount(models.Model):
     class Meta:
         verbose_name = 'حساب بانکی'
         verbose_name_plural = 'حساب‌های بانکی'
+        ordering = ('-verified', )
 
         unique_together = [('iban', 'user')]
 
