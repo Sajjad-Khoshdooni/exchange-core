75b1b29a348b4e131116423a54387ba7ea7fd7a1
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-03-30 10:49:48 +0200

    fix queue

--- provider/tasks/auto_hedge.py
+++ provider/tasks/auto_hedge.py
@@ -4,7 +4,7 @@ from ledger.models import Asset
 from provider.models import ProviderOrder
 
 
-@shared_task()
+@shared_task(queue='binance')
 def auto_hedge_assets():
     for asset in Asset.objects.all():
         ProviderOrder.try_hedge_for_new_order(asset, ProviderOrder.HEDGE)
