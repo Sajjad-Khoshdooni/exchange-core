5caa69909c72660084d5a0ba7f1f64280662173d
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-03-30 10:53:16 +0200

    fix bug

--- provider/tasks/auto_hedge.py
+++ provider/tasks/auto_hedge.py
@@ -6,6 +6,6 @@ from provider.models import ProviderOrder
 
 @shared_task(queue='binance')
 def auto_hedge_assets():
-    for asset in Asset.objects.all():
+    for asset in Asset.objects.exclude(symbol__in=[Asset.IRT, Asset.USDT]):
         ProviderOrder.try_hedge_for_new_order(asset, ProviderOrder.HEDGE)
 
