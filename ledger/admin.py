e35c986080b6cb366fbe0392af34c9174411293c
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-03-30 09:37:38 +0200

    add asset search in transfer

--- ledger/admin.py
+++ ledger/admin.py
@@ -259,7 +259,7 @@ class TransferUserFilter(SimpleListFilter):
 @admin.register(models.Transfer)
 class TransferAdmin(admin.ModelAdmin):
     list_display = ('created', 'network', 'wallet', 'amount', 'fee_amount', 'deposit', 'status', 'is_fee', 'source')
-    search_fields = ('trx_hash', 'block_hash', 'block_number', 'out_address')
+    search_fields = ('trx_hash', 'block_hash', 'block_number', 'out_address', 'wallet__asset__symbol')
     list_filter = ('deposit', 'status', 'is_fee', 'source', 'status', TransferUserFilter,)
 
 
