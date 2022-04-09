2e93fc3d35e50436398733f5bd64902772272aed
armin mohammadi <armin.mohamady@gmail.com>
2022-04-09 09:32:49 +0200

    minor bug fixed

--- market/views/order_book.py
+++ market/views/order_book.py
@@ -47,7 +47,7 @@ class OrderBookAPIView(APIView):
         last_trade = FillOrder.get_last(symbol=symbol)
 
         results = {
-            'last_trade': last_trade.format_values() if last_trade else None,
+            'last_trade': last_trade.format_values() if last_trade else {'amount': 0, 'price': 0, 'total': 0},
             'bids': filtered_bids[:20],
             'asks': asks[:20],
         }
