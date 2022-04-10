d10f03d108c068bd341aeaf0cdd65e2169b19c38
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-04-10 14:13:53 +0200

    add Payir class

---
+++ financial/utils/pay_ir.py
@@ -0,0 +1,38 @@
+import logging
+
+import requests
+from yekta_config import secret
+from yekta_config.config import config
+
+logger = logging.getLogger(__name__)
+
+
+class Payir:
+
+    @classmethod
+    def collect_api(cls, url: str, method: str = 'GET', data: dict = None):
+
+        request_kwargs = {
+            'url': url,
+            'timeout': 60,
+            'headers': {'Authorization': 'Bearer ' + secret('PAY_IR_TOKEN')},
+            'proxies': {
+                'http': config('IRAN_PROXY_IP', default='localhost') + ':3128',
+            }
+        }
+
+        try:
+            if method == 'GET':
+                resp = requests.get(params=data, **request_kwargs)
+            else:
+                method_prop = getattr(requests, method.lower())
+                resp = method_prop(data=data, **request_kwargs)
+        except requests.exceptions.ConnectionError:
+            logger.error('pay.ir connection error', extra={
+                'url': url,
+                'method': method,
+                'data': data,
+            })
+            raise TimeoutError
+
+        return resp.json()
