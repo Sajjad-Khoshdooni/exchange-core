4bec348ded8845556f522e09fa90ef12adbd0172
armin mohammadi <armin.mohamady@gmail.com>
2022-04-12 12:05:57 +0430

    minor bug fixed

--- market/models/order.py
+++ market/models/order.py
@@ -284,7 +284,7 @@ class Order(models.Model):
     def quantize_values(symbol: PairSymbol, open_orders):
         return [{
             'side': order['side'],
-            'price': order['price'],
+            'price': floor_precision(order['price'], symbol.tick_size),
             'unfilled_amount': floor_precision(order['unfilled_amount'], symbol.step_size),
         } for order in open_orders]
 
