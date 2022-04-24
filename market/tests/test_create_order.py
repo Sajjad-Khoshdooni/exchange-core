be3550434bce7705e280a521712768b9599e4b63
moha <mhfarani@gmail.com>
2022-04-24 18:45:41 +0430

    fix bug

--- market/tests/test_create_order.py
+++ market/tests/test_create_order.py
@@ -66,7 +66,7 @@ class CreateOrderTestCase(TestCase):
     def test_fill_order_with_different_price(self):
         order_3 = new_order(self.btcirt, Account.system(), 2, 200000, Order.SELL)
         order_4 = new_order(self.btcirt, self.account, 2, 200010, Order.BUY)
-        order_3.refresh_from_db()
+        order_3.refresh_from_db(), order_4.refresh_from_db()
 
         fill_order = FillOrder.objects.get(maker_order=order_3)
 
