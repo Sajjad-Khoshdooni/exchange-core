6badeed964a414157028eb47ffde994758c0aa72
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-04-30 16:43:20 +0200

    change con market cap fill

--- collector/models/coin_market_cap.py
+++ collector/models/coin_market_cap.py
@@ -24,7 +24,6 @@ class CoinMarketCap(models.Model):
     cmc_rank = models.IntegerField()
     circulating_supply = models.FloatField()
 
-
     @classmethod
     def request(cls):
         return requests.get(data_url).json()['data']['cryptoCurrencyList']
@@ -62,10 +61,10 @@ class CoinMarketCap(models.Model):
                     internal_id=coin['id'],
                     price=price_info['price'],
                     market_cap=price_info['marketCap'],
-                    change_1h=price_info['"percentChange1h'],
-                    change_24h=price_info['percentChange24h'],
+                    change_1h=price_info.get('"percentChange1h', 0),
+                    change_24h=price_info.get('percentChange24h', 0),
                     volume_24h=price_info['volume24h'],
-                    change_7d=price_info['percentChange7d'],
+                    change_7d=price_info.get('percentChange7d'),
                     high_24h=price_info['high24h'],
                     low_24h=price_info['low24h'],
                     cmc_rank=price_info['cmcRank'],
