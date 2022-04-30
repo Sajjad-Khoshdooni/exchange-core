ab9c6638921e4bfb04981a9e2de32bdadb87abba
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-04-30 13:32:36 +0200

    fix tests

--- accounts/tests/test_verification_level_2.py
+++ accounts/tests/test_verification_level_2.py
@@ -17,15 +17,31 @@ class VerificationLevel2TestCase(TestCase):
 
     def test_success_verify(self):
         self.client.force_login(self.user)
-        resp = self.client.post('/api/v1/accounts/verify/basic/', {
+
+        data = {
             'first_name': 'علی',
             'last_name': 'صالحی',
             'birth_date': '2000-01-01',
             'national_code': '1230046917',
             'card_pan': '6104337574599260',
             'iban': 'IR369271754583789114976929',
-        })
+        }
+
+        resp = self.client.post('/api/v1/accounts/verify/basic/', data)
         self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.get('/api/v1/accounts/verify/basic/')
+
+        for key, value in data.items():
+            resp_val = resp.data[key]
+
+            if key == 'card_pan':
+                resp_val = resp_val['card_pan']
+
+            if key == 'iban':
+                resp_val = resp_val['iban']
+
+            self.assertEqual(resp_val, value)
 
     def test_error_age(self):
         self.client.force_login(self.user)
