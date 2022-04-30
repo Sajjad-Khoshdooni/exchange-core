c857de0123750ed9bcf0cfe9f6d482f37fefcffa
moha <mhfarani@gmail.com>
2022-04-30 17:58:12 +0430

    fix bug

--- collector/tasks/coin_market_cap.py
+++ collector/tasks/coin_market_cap.py
@@ -18,5 +18,12 @@ def update_coin_market_cap():
 
             obj.change_24h = price_info['percentChange24h']
             obj.volume_24h = price_info['volume24h']
+            obj.market_cap = price_info['marketCap']
+            obj.change_1h = price_info['percentChange1h']
+            obj.change_7d = price_info['percentChange7d']
+            obj.high_24h = data['high24h']
+            obj.low_24h = data['low24h']
+            obj.cmc_rank = data['cmcRank']
+            obj.circulating_supply = data['circulatingSupply']
 
     CoinMarketCap.objects.bulk_update(objects, fields=['change_24h', 'volume_24h'])
