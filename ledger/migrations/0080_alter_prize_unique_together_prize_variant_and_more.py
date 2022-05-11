3e97120cfc5ea0d37488680da1a2d7e0ea079732
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-05-11 12:06:16 +0200

    fix migrations

--- ledger/migrations/0080_alter_prize_unique_together_prize_variant_and_more.py
+++ ledger/migrations/0080_alter_prize_unique_together_prize_variant_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0047_merge_20220510_1905'),
+        ('accounts', '0047_merge_20220511_1130'),
         ('ledger', '0079_prize_fake'),
     ]
 
