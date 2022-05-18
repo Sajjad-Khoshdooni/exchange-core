82d4c54eb419714b03fae0dd2e40190ce0b8ec31
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-05-18 12:48:27 +0200

    otc info

--- ledger/views/__init__.py
+++ ledger/views/__init__.py
@@ -1,7 +1,7 @@
 from .asset_info_view import AssetsViewSet
 from .wallet_view import WalletViewSet, WalletBalanceView, BriefNetworkAssetsView
 from .deposit_address_view import DepositAddressView
-from .otc_trade_view import OTCTradeRequestView, OTCTradeView, OTCHistoryView
+from .otc_trade_view import OTCTradeRequestView, OTCTradeView, OTCHistoryView, OTCInfoView
 from .margin_view import MarginInfoView, AssetMarginInfoView, MarginTransferViewSet, MarginLoanViewSet
 from .withdraw_view import WithdrawView
 from .transactions_history_view import WithdrawHistoryView, DepositHistoryView
