56280ee0bfa5ec58f179308523e9bdfdcfc3e4b4
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-05-21 14:35:48 +0200

    fix bug

--- accounts/migrations/0045_account_trade_volume_irt.py
+++ accounts/migrations/0045_account_trade_volume_irt.py
@@ -23,8 +23,8 @@ def populate_trade_volume_irt(apps, schema_editor):
     accounts = Account.objects.filter(id__in=set(maker_values) | set(taker_values))
     for account in accounts:
         account.trade_volume_irt = maker_values.get(account.id, 0) + taker_values.get(account.id, 0)
-        account.refresh_from_db()
         account.save(update_fields=['trade_volume_irt'])
+        account.refresh_from_db()
 
     for account in Account.objects.filter(trade_volume_irt__gte=2_000_000):
         Prize.objects.get_or_create(account=account, scope='trade_2m', defaults={'amount': 0, 'asset': asset, 'fake': True})
