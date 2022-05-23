fd6c9bb295061f3514e0bbfb4390e04cd652d021
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-05-23 17:35:24 +0200

    add fix market symbols

---
+++ market/utils/fix.py
@@ -0,0 +1,45 @@
+from ledger.models import Asset
+from ledger.utils.precision import get_precision
+from market.models import PairSymbol
+
+
+def create_symbol_from_pair(asset: Asset, base_asset: Asset) -> PairSymbol:
+    symbol, _ = PairSymbol.objects.get_or_create(
+        asset=asset, base_asset=base_asset, defaults={'name': f'{asset.symbol}{base_asset.symbol}'}
+    )
+    symbol.tick_size = asset.price_precision_irt if base_asset.symbol == 'IRT' else asset.price_precision_usdt
+    symbol.step_size = get_precision(asset.trade_quantity_step)
+    symbol.min_trade_quantity = asset.min_trade_quantity
+    symbol.max_trade_quantity = asset.max_trade_quantity
+    symbol.maker_quantity = min(10000 * asset.min_trade_quantity, asset.max_trade_quantity / 100)
+    symbol.save()
+
+    return symbol
+
+
+def create_symbols_for_asset(asset: Asset):
+    if asset.symbol == Asset.IRT:
+        return
+
+    irt_asset = Asset.objects.get(symbol=Asset.IRT)
+    usdt_asset = Asset.objects.get(symbol=Asset.USDT)
+
+    base_assets = [irt_asset, usdt_asset]
+
+    if asset.symbol == Asset.USDT:
+        base_assets = [irt_asset]
+
+    for base_asset in base_assets:
+        create_symbol_from_pair(asset, base_asset)
+
+
+def create_missing_symbols():
+    irt_asset = Asset.objects.get(symbol='IRT')
+    usdt_asset = Asset.objects.get(symbol='USDT')
+
+    for asset in Asset.objects.exclude(symbol='IRT'):
+        for base_asset in (irt_asset, usdt_asset):
+            if asset.symbol == 'USDT' and base_asset.symbol == 'USDT':
+                continue
+
+            create_symbol_from_pair(asset, base_asset)
