b8a156e9c2f9634b462daefef9f558a519b8cbb5
sajad <sijalkhf@gmail.com>
2022-06-21 13:03:42 +0430

    add  public address generator requester

--- ledger/models/deposit_address.py
+++ ledger/models/deposit_address.py
@@ -17,33 +17,28 @@ class DepositAddress(models.Model):
 
     @classmethod
     def new_deposit_address(cls, account, network):
-        if DepositAddress.objects.filter(address_key__account=account).exists():
-            return DepositAddress.objects.get(address_key__account=account)
+        address, address_key = None, None
 
-        address = AddressRequester().create_wallet()
-        address_key = AddressKey.objects.create(
-            account=account,
-            address=address
-        )
+        if DepositAddress.objects.filter(address_key__account=account, network=network).exists():
+            return DepositAddress.objects.get(address_key__account=account, network=network)
+
+        elif not DepositAddress.objects.filter(address_key__account=account).exists():
+            address = AddressRequester().create_wallet()
+            address_key = AddressKey.objects.create(
+                account=account,
+                address=address
+            )
+        else:
+            address_key = AddressKey.objects.get(account=account)
+            address = address_key.address
 
         return DepositAddress.objects.create(
             network=network,
             address_key=address_key,
-            address=get_presentation_address(network=network, base16_address=address),
+            address=AddressRequester().generate_public_address(network=network, address=address),
         )
-
-    # @property
-    # def account(self):
-    #     return self.account
 
     class Meta:
         unique_together = (
             ('network', 'address'),
         )
-
-
-def get_presentation_address(network, base16_address):
-    if network.symbol == 'BSC' or network.symbol == 'ETH':
-        return base16_address
-    elif network.symbol == 'TRX':
-        return get_base58_address('41' + base16_address[2:])
--- ledger/requester/address_requester.py
+++ ledger/requester/address_requester.py
@@ -3,13 +3,12 @@ import random
 
 import requests
 
-from ledger.requester.consts import MASTERKEY_WALLET_URL, MASTERKEY_TOKEN
+from ledger.requester.consts import MASTERKEY_WALLET_URL, MASTERKEY_TOKEN, MASTERKEY_PUBLIC_ADDRESS_GENERATOR_URL
 from django.conf import settings
 
 
 class AddressRequester:
     def __init__(self):
-        self.url = MASTERKEY_WALLET_URL
         self.header = {
             'Authorization': 'Token ' + MASTERKEY_TOKEN
         }
@@ -20,6 +19,14 @@ class AddressRequester:
         data = {
             'tag': 'tag'
         }
-        res = requests.post(url=self.url, data=data, headers=self.header)
+        res = requests.post(url=MASTERKEY_WALLET_URL, data=data, headers=self.header)
         response = json.loads(res.content.decode('utf-8'))
         return response['address']
+
+    def generate_public_address(self, address, network):
+        data = {
+            "address": address,
+            "network": network
+        }
+        response = requests.get(url=MASTERKEY_PUBLIC_ADDRESS_GENERATOR_URL, data=data, headers=self.header)
+        return response['public_address']
