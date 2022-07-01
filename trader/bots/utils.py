76e8f92549dbf7561ec7ec5bc923a33f416b4d6e
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-07-01 15:38:06 +0430

    decrease order size

--- trader/bots/utils.py
+++ trader/bots/utils.py
@@ -35,7 +35,7 @@ def min_order_value(base_symbol: str):
 
 def random_min_order_value(base_symbol: str) -> Decimal:
     min_order = min_order_value(base_symbol)
-    return Decimal(random.randint(2 * min_order, 40 * min_order))
+    return Decimal(random.randint(2 * min_order, 5 * min_order))
 
 
 def random_buy(symbol: PairSymbol, account: Account):
