7cba7056da34a4d94c6066e900650ff7e28ab461
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-07-04 09:00:03 +0200

    fix bug

--- accounts/models/user.py
+++ accounts/models/user.py
@@ -223,6 +223,11 @@ class User(AbstractUser):
     def reject_level2_if_should(self) -> bool:
 
         if self.level == User.LEVEL1 and self.verify_status == self.PENDING:
+            if User.objects.exclude(id=self.id).filter(national_code=self.national_code, level__gt=User.LEVEL1).exists():
+                self.national_code_duplicated_alert = True
+                self.save()
+                self.change_status(User.REJECTED)
+
             level2_fields = self.get_level2_verify_fields()
             any_none = list(filter(lambda f: f is None, level2_fields))
 
@@ -258,8 +263,8 @@ class User(AbstractUser):
                     self.change_status(self.REJECTED)
 
         elif self.level == self.LEVEL1 and self.verify_status == self.PENDING:
-            self.verify_level2_if_not()
             self.reject_level2_if_should()
+            self.verify_level2_if_not()
 
         if old and old.selfie_image_verified is None and self.selfie_image_verified is False:
             Notification.send(
