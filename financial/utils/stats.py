26e5d82258da9b4def609877bee7754948600123
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-07-09 17:36:47 +0200

    fix bug

--- financial/utils/stats.py
+++ financial/utils/stats.py
@@ -3,5 +3,8 @@ from financial.utils.withdraw import ZibalChanel
 
 def get_total_fiat_irt():
     channel = ZibalChanel()
-    wallet = channel.get_wallet_data(channel.get_wallet_id())
-    return wallet.balance
+    if channel.is_active():
+        wallet = channel.get_wallet_data(channel.get_wallet_id())
+        return wallet.balance
+    else:
+        return 0
