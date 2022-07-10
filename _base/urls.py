0451c2a91db94bb11ad93999f23516aa6f68dc80
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-07-10 13:15:35 +0200

    fix bug

--- _base/urls.py
+++ _base/urls.py
@@ -1,9 +1,9 @@
-from decouple import config
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from yekta_config.config import config
 
 from accounts.views.dashboard import dashboard
 from rest_framework import permissions
