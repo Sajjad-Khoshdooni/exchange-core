74935ff86e7ce5e9cfcbae45c18800449e4365c9
sajad <sijalkhf@gmail.com>
2022-07-11 11:19:11 +0430

    fix bugs

--- collector/views.py
+++ collector/views.py
@@ -14,13 +14,13 @@ class CoinPriceView(APIView):
         side = request.GET['side']
         base = request.GET['base']
         if base == 'usdt':
-            res = get_trading_price_usdt(coin=coin, side=side)
+            price = get_trading_price_usdt(coin=coin, side=side)
         elif base == 'irt':
-            res = get_trading_price_irt(coin=coin, side=side)
+            price = get_trading_price_irt(coin=coin, side=side)
         else:
             return Response(404)
         return Response({
-            'price': res
+            'price': price
         }, 200)
 
 
