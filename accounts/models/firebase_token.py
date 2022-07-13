c19638839cc30355dbcf3cb6f29348699798b356
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-07-13 14:04:54 +0200

    fix migration

--- accounts/models/firebase_token.py
+++ accounts/models/firebase_token.py
@@ -9,8 +9,8 @@ class FirebaseToken(models.Model):
     STATE_3 = '3'
     STATE_4 = '4'
 
-    state_choice = ((STATE_1, 'token_between_2h_1d'), (STATE_2, 'Token_between_1d_7d'),
-                    (STATE_3, 'token_more_than_7d'))
+    state_choice = ((STATE_1, '2h'), (STATE_2, '1d'),
+                    (STATE_3, '7d'))
 
     created = models.DateTimeField(auto_now_add=True)
 
