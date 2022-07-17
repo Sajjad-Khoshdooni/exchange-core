154ead4454da37035f7ae54829099c9e1ca1fdab
moha <mhfarani@gmail.com>
2022-07-17 15:22:11 +0430

    add test for convert dust

--- ledger/tests/test_convert_dust.py
+++ ledger/tests/test_convert_dust.py
@@ -4,7 +4,10 @@ from django.test import Client
 from django.test import TestCase
 
 from ledger.models import Asset
-from ledger.utils.test import new_account
+from ledger.utils.test import new_account, set_price
+
+BTC_USDT_PRICE = 20000
+USDT_IRT_PRICE = 30000
 
 
 class ConvertDustTestCase(TestCase):
@@ -23,30 +26,24 @@ class ConvertDustTestCase(TestCase):
 
         self.wallet_usdt = self.usdt.get_wallet(self.account)
         self.wallet_btc = self.btc.get_wallet(self.account)
-        self.wallet_ada = self.ada.get_wallet(self.account)
         self.wallet_irt = self.irt.get_wallet(self.account)
+
+        set_price(self.btc, BTC_USDT_PRICE)
+        set_price(self.usdt, USDT_IRT_PRICE)
 
     def test_convert_dust_for_zero_wallet(self):
         resp = self.client.post('/api/v1/convert/dust/')
 
-        self.wallet_ada.refresh_from_db()
-        self.wallet_btc.refresh_from_db()
-        self.wallet_usdt.refresh_from_db()
-        self.wallet_irt.refresh_from_db()
-
         self.assertEqual(resp.status_code, 200)
 
         self.assertEqual(self.wallet_irt.balance, 0)
-        self.assertEqual(self.wallet_ada.balance, 0)
         self.assertEqual(self.wallet_btc.balance, 0)
         self.assertEqual(self.wallet_usdt.balance, 0)
 
-    def test_test_convert_dust(self):
+    def test_convert_dust(self):
         self.wallet_btc.airdrop(Decimal('.000001'))
         self.wallet_usdt.airdrop(Decimal('10'))
-
-        print('btc balance before dust is {}'.format(self.wallet_btc.balance))
-        print('usdt balance before dust is {}'.format(self.wallet_usdt.balance))
+        self.wallet_irt.airdrop(Decimal('5000'))
 
         resp = self.client.post('/api/v1/convert/dust/')
 
@@ -54,15 +51,7 @@ class ConvertDustTestCase(TestCase):
         self.wallet_usdt.refresh_from_db()
         self.wallet_irt.refresh_from_db()
 
-        print('irt{}'.format(self.wallet_irt.balance))
-        print('btc{}'.format(self.wallet_btc.balance))
-        print('usdt{}'.format(self.wallet_usdt.balance))
-
         self.assertEqual(resp.status_code, 200)
-        self.assertGreater(self.wallet_irt.balance, 0)
+        self.assertGreater(self.wallet_irt.balance, 5000)
         self.assertEqual(self.wallet_usdt.balance, 10)
         self.assertEqual(self.wallet_btc.balance, 0)
-
-
-
-
