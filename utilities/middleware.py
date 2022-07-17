a50274d0886f3f4d0e286385ee2d1318acd4efbb
armin mohammadi <armin.mohamady@gmail.com>
2022-07-17 17:26:24 +0430

    fix locale activation bug

--- utilities/middleware.py
+++ utilities/middleware.py
@@ -8,8 +8,8 @@ class SetLocaleMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        response = self.get_response(request)
         if '/admin' in request.path:
             activate(settings.LANGUAGE_CODE)
 
+        response = self.get_response(request)
         return response
