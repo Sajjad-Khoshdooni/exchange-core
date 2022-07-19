068fac34dee46efa07d555ffd82fa81826e981b7
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-07-19 16:32:45 +0200

    fix bug

--- accounts/utils/push_notif.py
+++ accounts/utils/push_notif.py
@@ -20,9 +20,10 @@ def _get_access_token():
 def send_push_notif_to_user(user: User, title: str, body: str, image: str = None, link: str = None):
     from accounts.models import FirebaseToken
 
-    fire_base_token = FirebaseToken.objects.filter(user=user).last()
+    firebase_token = FirebaseToken.objects.filter(user=user).last()
 
-    return send_push_notif(fire_base_token.token,  title, body, image, link)
+    if firebase_token:
+        return send_push_notif(firebase_token.token,  title, body, image, link)
 
 
 def send_push_notif(token: str, title: str, body: str, image: str = None, link: str = None):
