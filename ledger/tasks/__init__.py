3266e601533bf6e26140cda8c1fed5a42fcf7dd8
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-07-25 07:17:32 +0200

    fix bug

--- ledger/tasks/__init__.py
+++ ledger/tasks/__init__.py
@@ -3,3 +3,4 @@ from .withdraw import create_transaction_from_not_broadcasts, create_binance_wit
     update_withdraws, create_withdraw
 from .fee import update_network_fees
 from .lock_monitor import lock_monitor
+from .register_address import register_address
