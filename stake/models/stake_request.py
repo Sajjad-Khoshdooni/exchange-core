f83722b1e2b6a73a082c35213c687775cfcdf911
moha <mhfarani@gmail.com>
2022-07-26 16:58:13 +0430

    fix bug

--- stake/models/stake_request.py
+++ stake/models/stake_request.py
@@ -7,7 +7,7 @@ from django.db import models
 from accounts.models import Account
 from accounts.utils import email
 from ledger.models import Wallet, Trx
-from ledger.utils.fields import get_group_id_field
+from ledger.utils.fields import get_group_id_field, get_amount_field
 from ledger.utils.wallet_pipeline import WalletPipeline
 from stake.models import StakeOption
 from stake.utils import change_status
@@ -18,20 +18,16 @@ class StakeRequest(models.Model):
     PROCESS, PENDING, DONE = 'process', 'pending', ' done'
     CANCEL_PROCESS, CANCEL_PENDING, CANCEL_COMPLETE = 'cancel_process', 'cancel_pending', 'cancel_complete'
 
-    status_choice = ((PROCESS, PROCESS), (PENDING, PENDING), (DONE, DONE), (CANCEL_PROCESS, CANCEL_PROCESS),
+    STATUS_CHOICE = ((PROCESS, PROCESS), (PENDING, PENDING), (DONE, DONE), (CANCEL_PROCESS, CANCEL_PROCESS),
                      (CANCEL_PENDING, CANCEL_PENDING), (CANCEL_COMPLETE, CANCEL_COMPLETE))
 
-    status = models.CharField(choices=status_choice, max_length=16, default=PROCESS)
+    status = models.CharField(choices=STATUS_CHOICE, max_length=16, default=PROCESS)
 
     stake_option = models.ForeignKey(StakeOption, on_delete=models.CASCADE)
 
-    amount = models.PositiveIntegerField()
+    amount = get_amount_field()
 
-    group_id = models.UUIDField(
-        default=uuid.uuid4,
-        unique=True,
-        db_index=True,
-    )
+    group_id = get_group_id_field()
 
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
