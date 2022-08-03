26b159b4c93d2360029f8d5509ed46bf9927be19
sajad <sijalkhf@gmail.com>
2022-08-03 12:26:58 +0430

    fix bug

--- ledger/tests/test_fast_forward.py
+++ ledger/tests/test_fast_forward.py
@@ -15,18 +15,19 @@ class FastForwardTestCase(TestCase):
         self.sender_account1 = new_account()
         self.sender_address_key1 = AddressKey.objects.create(account=self.sender_account1, address='0xC8E19189888BED6aaBf88800024106eC7C8cb00B')
         self.sender_deposit_address1 = DepositAddress.objects.create(
-            account=self.sender_account1,
+            # account=self.sender_account1,
             network=self.network,
             address_key=self.sender_address_key1,
             address='0xC8E19189888BED6aaBf88800024106eC7C8cb00B'
         )
         self.sender_wallet1 = self.asset.get_wallet(account=self.sender_account1)
-
+        self.sender_wallet1.balance = 10
+        self.sender_wallet1.save()
 
         self.receiver_account1 = new_account()
         self.receiver_address_key1 = AddressKey.objects.create(account=self.receiver_account1, address='0x701e0e2f85E3922F50C054d113b9D694c675a7f5')
         self.receiver_deposit_address1 = DepositAddress.objects.create(
-            account=self.receiver_account1,
+            # account=self.receiver_account1,
             network=self.network,
             address_key=self.receiver_address_key1,
             address='0x701e0e2f85E3922F50C054d113b9D694c675a7f5'
@@ -56,10 +57,10 @@ class FastForwardTestCase(TestCase):
         # self.receiver_wallet2 = self.asset.get_wallet(account=self.receiver_account2)
 
     def test_fast_forward1(self):
-        Transfer.new_withdraw(
-            wallet=self.sender_wallet1,
+        Transfer.check_fast_forward(
+            sender_wallet=self.sender_wallet1,
             network=self.network,
-            amount=Decimal(0),
+            amount=Decimal(1),
             address=self.receiver_deposit_address1.address
         )
 
@@ -68,14 +69,12 @@ class FastForwardTestCase(TestCase):
             Transfer.objects.get(deposit_address=self.receiver_deposit_address1).group_id)
 
     def test_fast_forward2(self):
-        Transfer.new_withdraw(
-            wallet=self.sender_wallet1,
+        transfer = Transfer.check_fast_forward(
+            sender_wallet=self.sender_wallet1,
             network=self.network,
-            amount=Decimal(0),
+            amount=Decimal(1),
             address='0x6D3251369E08248f7a37355E497682Fd465767fb'
         )
 
-        self.assertEqual(
-            Trx.objects.get(sender=self.sender_wallet1).group_id,
-            Transfer.objects.get(deposit_address='0x6D3251369E08248f7a37355E497682Fd465767fb').group_id)
-
+        self.assertEqual(transfer, None)
+#
