c5bc5ff44e85f8d3c0561da7e397c565dbe7fee2
armin mohammadi <armin.mohamady@gmail.com>
2022-08-06 14:32:33 +0430

    fix(pnl): separate spot and margin pnl overview

--- ledger/views/pnl_views.py
+++ ledger/views/pnl_views.py
@@ -2,14 +2,16 @@ from datetime import timedelta
 
 from django.db.models import Sum, Q
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from ledger.models import PNLHistory
+from ledger.models import PNLHistory, Wallet
 
 
 class PNLOverview(APIView):
@@ -17,17 +19,22 @@ class PNLOverview(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request: Request):
+        market = self.request.query_params.get('market')
+        if not market or market not in (Wallet.SPOT, Wallet.MARGIN):
+            raise ValidationError(_('valid market is required'))
         today = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
         profit_info = PNLHistory.objects.filter(
-            account=self.request.user.account, date__gt=today - timedelta(days=30)
-        ).aggregate(
+            account_id=9, date__gt=today - timedelta(days=30), market=market
+        ).values('base_asset').annotate(
             cumulative_profit_30=Sum('profit'),
             cumulative_profit_7=Sum('profit', filter=Q(date__gt=today - timedelta(days=7))),
             last_profit=Sum('profit', filter=Q(date__gte=today))
         )
 
         return Response({
-            '1': profit_info['last_profit'],
-            '7': profit_info['cumulative_profit_7'],
-            '30': profit_info['cumulative_profit_30'],
+            pnl['base_asset']: {
+                '1': pnl['last_profit'],
+                '7': pnl['cumulative_profit_7'],
+                '30': pnl['cumulative_profit_30'],
+            } for pnl in profit_info
         })
