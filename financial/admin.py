7a729d2cd19b9c43572bbf4eeaceb99b9ae9b1de
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-08-17 09:29:12 +0200

    fix minor

--- financial/admin.py
+++ financial/admin.py
@@ -185,7 +185,7 @@ class BankCardAdmin(SimpleHistoryAdmin, AdvancedAdmin):
 
     @admin.action(description='تایید خودکار شماره کارت')
     def verify_bank_cards(self, request, queryset):
-        for bank_card in queryset:
+        for bank_card in queryset.filter(kyc=False):
             verify_bank_card_task.delay(bank_card.id)
 
     @admin.action(description='تایید شماره کارت')
