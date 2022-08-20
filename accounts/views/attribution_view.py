23c54457a4ee856ef6686ef00dd3c495415e6453
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-08-20 14:05:48 +0200

    remove permission from attribution view

--- accounts/views/attribution_view.py
+++ accounts/views/attribution_view.py
@@ -3,5 +3,8 @@ from rest_framework.views import APIView
 
 
 class AttributionAPIView(APIView):
+    authentication_classes = []
+    permission_classes = []
+
     def get(self, request):
         return Response(status=200)
