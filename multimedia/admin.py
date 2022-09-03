19d4beb9df1853b1fe2d87808a938ed4562cc935
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-09-03 17:01:29 +0200

    add banner to admin

--- multimedia/admin.py
+++ multimedia/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from multimedia.models import Image
+from multimedia.models import Image, Banner
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'image')
     search_fields = ('uuid', )
+
+
+@admin.register(Banner)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image', 'link', 'active')
+    list_editable = ('active', )
