7348e990b0ee2f9a2658f6aed83e25a0bc3e2e62
moha <mhfarani@gmail.com>
2022-09-03 14:32:23 +0430

    get bank_card_id in fast_buy_token api

--- ledger/views/fast_buy_token_view.py
+++ ledger/views/fast_buy_token_view.py
@@ -5,7 +5,7 @@ from rest_framework.generics import CreateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from accounts.views.authentication import CustomTokenAuthentication
-from financial.models import PaymentRequest
+from financial.models import PaymentRequest, BankCard
 from financial.views.payment_view import PaymentRequestSerializer
 from ledger.models import Asset
 from ledger.models.asset import CoinField
@@ -15,7 +15,7 @@ from ledger.utils.price import get_price
 
 class FastBuyTokenSerializer(serializers.ModelSerializer):
     coin = CoinField(source='asset')
-    card_pan = serializers.CharField(write_only=True)
+    card_pan_id = serializers.CharField(write_only=True)
     call_back = serializers.SerializerMethodField(read_only=True)
 
     def get_call_back(self, fast_buy_token: FastBuyToken):
@@ -31,15 +31,17 @@ class FastBuyTokenSerializer(serializers.ModelSerializer):
         request = self.context['request']
         payment_request_serializer = PaymentRequestSerializer()
         payment_request_serializer.context['request'] = request
+        validated_data['card_pan'] = BankCard.objects.get(id=validated_data['card_pan_id']).card_pan
         validated_data['payment_request'] = payment_request_serializer.create(validated_data)
         validated_data['user'] = request.user
+        validated_data.pop('card_pan_id')
         validated_data.pop('card_pan')
         validated_data['price'] = get_price(coin=validated_data['asset'].symbol, side='sell') or 0
         return super().create(validated_data)
 
     class Meta:
         model = FastBuyToken
-        fields = ('coin', 'amount', 'card_pan', 'call_back')
+        fields = ('coin', 'amount', 'card_pan_id', 'call_back')
 
 
 class FastBuyTokenAPI(CreateAPIView):
