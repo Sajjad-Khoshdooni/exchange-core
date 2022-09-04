d7b55621f5d700a3952b6fb0524b55305660583b
armin mohammadi <armin.mohamady@gmail.com>
2022-09-04 17:33:57 +0430

    change log msg

--- trader/tasks/random_trader.py
+++ trader/tasks/random_trader.py
@@ -44,7 +44,7 @@ def get_account():
 
 
 def random_trade(symbol: PairSymbol, account, top_amounts, daily_factor: int):
-    logger.info('random trading %s' % symbol)
+    logger.info(f'random trading {symbol} ({top_amounts})')
 
     random_func, max_amount = \
         random.choices([(random_buy, top_amounts[Order.SELL]), (random_sell, top_amounts[Order.BUY])])[0]
