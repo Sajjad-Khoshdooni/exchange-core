a1ee5f787da94dab13df45495fef154a219b2743
moha <mhfarani@gmail.com>
2022-09-10 17:58:13 +0430

    add buy and sell in accounting

--- accounting/task/create_daily_fiat_transfer.py
+++ accounting/task/create_daily_fiat_transfer.py
@@ -8,6 +8,8 @@ from financial.models import Payment, Gateway, FiatWithdrawRequest
 from ledger.utils.fields import DONE
 import pandas as pd
 
+from market.models import Trade, Order
+
 
 def send_document(file_path: str):
     f = open(file_path, 'rb')
@@ -32,31 +34,65 @@ def send_document(file_path: str):
     return resp.json()
 
 
-@shared_task(queue='')
-def create_daily_transfer():
+def edit_payment_daily_dict(daily_payment_dict:dict, date):
     gateways = Gateway.objects.all()
 
-    data = {'date': [], 'gateway': [], 'withdraw': [], 'deposit': []}
-    for i in range(7, 0, -1):
+    for gateway in gateways:
+        deposit = Payment.objects.filter(
+            created__date=date, payment_request__gateway=gateway, status=DONE,
+        ).aggregate(Sum('payment_request__amount'))['payment_request__amount__sum'] or 0
 
+        withdraw = FiatWithdrawRequest.objects.filter(
+            created__date=date, status=DONE, withdraw_channel=gateway.name
+        ).aggregate(Sum('amount'))['amount__sum'] or 0
+
+        if withdraw or deposit:
+            daily_payment_dict['date'].append(date)
+            daily_payment_dict['deposit'].append(deposit * 10)
+            daily_payment_dict['withdraw'].append(withdraw * 10)
+            daily_payment_dict['gateway'].append(gateway.name)
+
+    return daily_payment_dict
+
+
+def edit_daily_trade_dict(daily_trade_dict: dict, date):
+    sell_trade = Trade.objects.filter(
+        created__date=date,
+        trade_source__in=(Trade.SYSTEM, Trade.SYSTEM_TAKER, Trade.SYSTEM_MAKER),
+        side=Order.SELL
+    ).aggregate(Sum('irt_value'))['irt_value__sum'] or 0
+
+    buy_trade = Trade.objects.filter(
+        created__date=date,
+        trade_source__in=(Trade.SYSTEM, Trade.SYSTEM_TAKER, Trade.SYSTEM_MAKER),
+        side=Order.BUY
+    ).aggregate(Sum('irt_value'))['irt_value__sum'] or 0
+
+    if sell_trade or buy_trade:
+        daily_trade_dict['date'].append(date)
+        daily_trade_dict['sell'].append(sell_trade * 10)
+        daily_trade_dict['buy'].append(buy_trade * 10)
+
+
+@shared_task(queue='')
+def create_daily_transfer():
+
+    daily_payment_dict = {'date': [], 'gateway': [], 'withdraw': [], 'deposit': []}
+    daily_trade_dict = {'date': [], 'sell': [], 'buy': []}
+
+    for i in range(7, -1, -1):
         date = (timezone.now() - timedelta(days=i)).date()
+        edit_payment_daily_dict(daily_payment_dict, date)
+        edit_daily_trade_dict(daily_trade_dict, date)
 
-        for gateway in gateways:
-            deposit = Payment.objects.filter(
-                created__date=date, payment_request__gateway=gateway, status=DONE,
-            ).aggregate(Sum('payment_request__amount'))['payment_request__amount__sum'] or 0
+    daily_payment_data_frame = pd.DataFrame(daily_payment_dict)
+    daily_payment_data_frame.to_csv('daily_payment_{}.csv'.format(date), index=False)
 
-            withdraw = FiatWithdrawRequest.objects.filter(
-                created__date=date, status=DONE, withdraw_channel=gateway.name
-            ).aggregate(Sum('amount'))['amount__sum'] or 0
-
-            if withdraw or deposit:
-                data['date'].append(date)
-                data['deposit'].append(deposit)
-                data['withdraw'].append(withdraw)
-                data['gateway'].append(gateway.name)
-
-    data_frame = pd.DataFrame(data)
-    data_frame.to_csv('daily_payment_{}.csv'.format(date), index=False)
+    daily_trade_data_frame = pd.DataFrame(daily_trade_dict)
+    daily_trade_data_frame.to_csv('daily_trade_{}.csv'.format(date), index=False)
 
     send_document(file_path='daily_payment_{}.csv'.format(date))
+    send_document(file_path='daily_trade_{}.csv'.format(date))
+
+
+#check irt
