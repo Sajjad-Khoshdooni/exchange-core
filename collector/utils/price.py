b918b817935816d66f6546b38f489baeabc5080b
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-10-18 16:45:01 +0330

    fix bug

--- collector/utils/price.py
+++ collector/utils/price.py
@@ -2,4 +2,4 @@ from redis import Redis
 
 from django.conf import settings
 
-price_redis = Redis.from_url(settings.PROVIDER_CACHE_LOCATION, decode_responses=True)
+price_redis = Redis.from_url(settings.PRICE_CACHE_LOCATION, decode_responses=True)
