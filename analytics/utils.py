bccd65584071947467a275409b14985b7331b32b
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-10-23 14:13:36 +0330

    fix bug

--- analytics/utils.py
+++ analytics/utils.py
@@ -23,11 +23,23 @@ def produce_users_analytics(user_ids: list):
 
     trades = Trade.objects.filter(order__wallet__account__user_id__in=users)
 
-    return {
+    data = {
         'users': len(user_ids),
         'verified': users.filter(level__gt=User.LEVEL1).count(),
-        'deposited': with_deposit_users,
+        'deposited': len(with_deposit_users),
         'deposit_value': fiat_deposit_volume + crypto_deposit_volume,
         'traders': len(set(trades.values_list('order__wallet__account__user_id', flat=True))),
         'trade_volume': trades.aggregate(value=Sum('irt_value'))['value'] or 0,
+        'trade_revenue': trades.aggregate(value=Sum('gap_revenue'))['value'] or 0,
     }
+
+    data.update({
+        'verified_conv': round(float(data['verified'] * 100) / data['users'], 2),
+        'deposited_conv': round(float(data['deposited'] * 100) / data['users'], 2),
+        'trader_conv': round(float(data['traders'] * 100) / data['users'], 2),
+        'deposit_value_per_user': round(float(data['deposit_value']) / data['users'], 2),
+        'trade_volume_per_user': round(float(data['trade_volume']) / data['users'], 2),
+        'trade_revenue_per_user': round(float(data['trade_revenue']) / data['users'], 2),
+    })
+
+    return data
