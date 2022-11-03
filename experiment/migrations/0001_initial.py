6d107198313a4524366a2814271d1af77035d681
Reza Abbasi <reza.abbasi.sut@gmail.com>
2022-11-03 18:44:45 +0330

    fix bug

--- experiment/migrations/0001_initial.py
+++ experiment/migrations/0001_initial.py
@@ -51,7 +51,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created', models.DateTimeField(auto_now_add=True)),
-                ('user_agent', models.TextField(blank=True, max_length=500)),
+                ('user_agent', models.TextField(blank=True)),
                 ('link', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='experiment.link')),
             ],
         ),
