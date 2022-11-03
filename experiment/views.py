53d160c59edc7353fa9e282833ac75fdb470e5cd
Sajad <Sijalkhf@gmail.com>
2022-11-03 15:52:35 +0330

    fix

--- experiment/views.py
+++ experiment/views.py
@@ -7,9 +7,7 @@ from experiment.models.link import Link
 def deposit_experiment_view(request, token):
     link = Link.objects.filter(token=token).first()
     if link:
-        click = Click.objects.create(user_agent=str(request.user_agent))
-        link.click = click
-        link.save(update_fields=['click'])
+        Click.objects.create(user_agent=str(request.user_agent), link=link)
 
         response = redirect(link.destination)
         return response
