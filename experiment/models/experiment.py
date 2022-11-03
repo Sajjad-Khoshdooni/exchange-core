bf7c2ed93483dbece36d24d3fb8d28a02ec32d25
Sajad <Sijalkhf@gmail.com>
2022-11-03 15:46:01 +0330

    fix

--- experiment/models/experiment.py
+++ experiment/models/experiment.py
@@ -5,6 +5,6 @@ class Experiment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=20)
-    a_variant = models.ForeignKey('experiment.Variant', on_delete=models.CASCADE, blank=True, related_name='VARIANT_A')
+    a_variant = models.ForeignKey('experiment.Variant', on_delete=models.CASCADE, null=True, blank=True, related_name='VARIANT_A')
     b_variant = models.ForeignKey('experiment.Variant', on_delete=models.CASCADE, related_name='VARIANT_B')
     active = models.BooleanField(default=True, db_index=True)
