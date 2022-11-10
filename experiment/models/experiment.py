934ae2669b093f9dd9f1309f4fce1be8e95b3beb
Sajad <sijalkhf@gmail.com>
2022-11-10 17:54:45 +0330

    add html field

--- experiment/models/experiment.py
+++ experiment/models/experiment.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
+from analytics.utils import produce_users_analytics
+from experiment.models.variant import Variant
+from experiment.models.variant_user import VariantUser
 
 
 class Experiment(models.Model):
@@ -9,3 +16,27 @@ class Experiment(models.Model):
 
     def __str__(self):
         return self.name
+
+    @admin.display
+    def get_variants_result(self):
+        variant_list = Variant.objects.filter(experiment=self)
+        html_url = '<table><tr><th>Parameters</th>'
+        for variant in variant_list:
+            html_url += format_html("<th>{}</th>", variant.name)
+        html_url += '</tr>'
+
+        user_analytics_list = [
+            produce_users_analytics(VariantUser.objects.filter(variant=variant).values_list('user__id', flat=True))
+            for variant in variant_list
+        ]
+        for key in user_analytics_list[0].keys():
+            html_url += '<tr><td>{}</td>'.format(key)
+
+            for user_analytics in user_analytics_list:
+                html_url += format_html("<td>{}</td>", user_analytics.get(key))
+
+            html_url += '</tr>'
+
+        html_url += '</table>'
+
+        return mark_safe(html_url)
