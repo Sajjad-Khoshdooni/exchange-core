09c4d05c2df5b082b402e03215a8034f9b2d0728
armin mohammadi <armin.mohamady@gmail.com>
2022-11-28 17:34:43 +0330

    fix(ohlc): minor change in parse

--- market/views/tradingview_views.py
+++ market/views/tradingview_views.py
@@ -112,9 +112,9 @@ class OHLCVAPIView(APIView):
         start = request.query_params.get('from', (timezone.now() - timedelta(hours=24)).timestamp())
         end = request.query_params.get('to', timezone.now().timestamp())
         interval = request.query_params.get('resolution')
-        if str(interval) == '15':
-            interval = 15 * 60
-        elif str(interval) in ('1', '1h', '1H'):
+        if str(interval).isdigit():
+            interval = int(interval) * 60
+        elif str(interval) in ('1h', '1H'):
             interval = 3600
         elif str(interval) in ('1d', '1D'):
             interval = 24 * 3600
