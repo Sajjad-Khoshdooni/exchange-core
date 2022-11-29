5269a8df9d4ad8f493f55d75a3549ef356eb5139
Reza <reza.abbasi.sut@gmail.com>
2022-11-29 11:19:00 +0330

    make ip_list nullable

--- accounts/models/custom_token.py
+++ accounts/models/custom_token.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class CustomToken(Token):
     ip_list = ArrayField(
-        models.GenericIPAddressField(default='127.0.0.1'), default=list
+        models.GenericIPAddressField(default='127.0.0.1'), default=list, blank=True, null=True
     )
