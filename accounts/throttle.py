6a90976aa9774eb822c3c7fb8ea27effdf236111
armin mohammadi <armin.mohamady@gmail.com>
2022-12-20 14:59:46 +0330

    ignore throttle for delegated requests

--- accounts/throttle.py
+++ accounts/throttle.py
@@ -1,17 +1,28 @@
 from rest_framework.throttling import UserRateThrottle
 
+from accounts.views.jwt_views import user_has_delegate_permission
 
-class BurstRateThrottle(UserRateThrottle):
+
+class CustomUserRateThrottle(UserRateThrottle):
+    def allow_request(self, request, view):
+        if request.auth and request.user and user_has_delegate_permission(request.user) and \
+                getattr(request.auth, 'token_type', None) == 'access' and \
+                hasattr(request.auth, 'payload') and request.auth.payload.get('account_id'):
+            return self.throttle_success()
+        return super(CustomUserRateThrottle, self).allow_request(request, view)
+
+
+class BurstRateThrottle(CustomUserRateThrottle):
     scope = 'burst'
 
 
-class SustainedRateThrottle(UserRateThrottle):
+class SustainedRateThrottle(CustomUserRateThrottle):
     scope = 'sustained'
 
 
-class BursApiRateThrottle(UserRateThrottle):
+class BursApiRateThrottle(CustomUserRateThrottle):
     scope = 'burst_api'
 
 
-class SustaineApiRatethrottle(UserRateThrottle):
+class SustaineApiRatethrottle(CustomUserRateThrottle):
     scope = 'sustained_api'
