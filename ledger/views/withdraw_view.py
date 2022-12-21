3f2bb3fa599fd6561368c4efc4b8e780d0dfdc56
armin mohammadi <armin.mohamady@gmail.com>
2022-12-21 12:06:45 +0330

    add ref name to duplicate serializer names

--- ledger/views/withdraw_view.py
+++ ledger/views/withdraw_view.py
@@ -148,6 +148,7 @@ class WithdrawSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transfer
         fields = ('amount', 'address', 'coin', 'network', 'code', 'address_book_id', 'memo', 'code_2fa')
+        ref_name = 'Withdraw Serializer'
 
 
 class WithdrawView(CreateAPIView):
