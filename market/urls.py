1b0865a51ba1aa80d24c8bd5ab5a52b01d10210b
Reza <reza.abbasi.sut@gmail.com>
2022-12-29 12:34:00 +0330

    add caching

--- market/urls.py
+++ market/urls.py
@@ -12,12 +12,12 @@ urlpatterns = [
     path('irt/info/', cache_page(60)(MarketInfoView.as_view())),
     path('depth/<str:symbol>/', OrderBookAPIView.as_view()),
     path('orders/cancel/', CancelOrderAPIView.as_view()),
-    path('symbols/<str:name>/', SymbolDetailedStatsAPIView.as_view()),
+    path('symbols/<str:name>/', cache_page(300)(SymbolDetailedStatsAPIView.as_view())),
     path('symbols/', cache_page(300)(SymbolListAPIView.as_view())),
     path('myTrades/', AccountTradeHistoryView.as_view()),
-    path('trades/', TradeHistoryView.as_view()),
-    path('tradingview/ohlcv/', OHLCVAPIView.as_view()),
+    path('trades/', cache_page(10)(TradeHistoryView.as_view())),
+    path('tradingview/ohlcv/', cache_page(10)(OHLCVAPIView.as_view())),
     path('open-orders/', OpenOrderListAPIView.as_view()),
     path('', include(router.urls)),
-    path('bookmark/', BookMarkSymbolAPIView.as_view())
+    path('bookmark/', BookMarkSymbolAPIView.as_view()),
 ]
