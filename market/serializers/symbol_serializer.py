39c4647541d463d2fbb2c968e8e88c8921697e98
Armin Mohammadi <armin.mohamady@gmail.com>
2023-01-27 18:11:31 +0330

    add market maker enabled to serializer

--- market/serializers/symbol_serializer.py
+++ market/serializers/symbol_serializer.py
@@ -36,7 +36,8 @@ class SymbolSerializer(serializers.ModelSerializer):
     class Meta:
         model = PairSymbol
         fields = ('name', 'asset', 'base_asset', 'taker_fee', 'maker_fee', 'tick_size', 'step_size',
-                  'min_trade_quantity', 'max_trade_quantity', 'enable', 'bookmark', 'margin_enable', 'strategy_enable')
+                  'min_trade_quantity', 'max_trade_quantity', 'enable', 'bookmark', 'margin_enable', 'strategy_enable',
+                  'market_maker_enabled')
 
 
 class SymbolBreifStatsSerializer(serializers.ModelSerializer):
@@ -89,7 +90,7 @@ class SymbolBreifStatsSerializer(serializers.ModelSerializer):
     class Meta:
         model = PairSymbol
         fields = ('name', 'asset', 'base_asset', 'enable', 'price', 'change_percent', 'bookmark', 'margin_enable',
-                  'strategy_enable')
+                  'strategy_enable', 'market_maker_enabled')
 
 
 class SymbolStatsSerializer(SymbolBreifStatsSerializer):
