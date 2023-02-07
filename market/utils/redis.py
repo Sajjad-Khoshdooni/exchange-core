277d345871dfaa1b3ba3703e8d13355296a14444
armin mohammadi <armin.mohamady@gmail.com>
2023-02-07 13:18:46 +0330

    fix set if lower script

--- market/utils/redis.py
+++ market/utils/redis.py
@@ -85,7 +85,7 @@ class MarketStreamCache:
 
     _funcs_dict = {
         SET_IF_HIGHER: "local c = tonumber(redis.call('get', KEYS[1])); if c then if tonumber(ARGV[1]) > c then redis.call('set', KEYS[1], ARGV[1]) return tonumber(ARGV[1]) - c else return 0 end else return redis.call('set', KEYS[1], ARGV[1]) end",
-        SET_IF_LOWER: "local c = tonumber(redis.call('get', KEYS[1])); if c then if tonumber(ARGV[1]) > c then redis.call('set', KEYS[1], ARGV[1]) return tonumber(ARGV[1]) - c else return 0 end else return redis.call('set', KEYS[1], ARGV[1]) end",
+        SET_IF_LOWER: "local c = tonumber(redis.call('get', KEYS[1])); if c then if tonumber(ARGV[1]) < c then redis.call('set', KEYS[1], ARGV[1]) return tonumber(ARGV[1]) - c else return 0 end else return redis.call('set', KEYS[1], ARGV[1]) end",
         SET_IF_NOT_EQUAL: "local c = tonumber(redis.call('get', KEYS[1])); if c then if tonumber(ARGV[1]) ~= c then redis.call('set', KEYS[1], ARGV[1]) return tonumber(ARGV[1]) - c else return 0 end else return redis.call('set', KEYS[1], ARGV[1]) end",
     }
 
