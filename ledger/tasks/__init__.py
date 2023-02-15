8278ac7ef998d20c3f9142474b02e71497f149d5
Reza <reza.abbasi.sut@gmail.com>
2023-02-15 22:53:42 +0330

    fix bugs

--- ledger/tasks/__init__.py
+++ ledger/tasks/__init__.py
@@ -3,4 +3,4 @@ from .withdraw import create_provider_withdraw, update_provider_withdraw, update
 from .fee import update_network_fees
 from .pnl import create_pnl_histories
 from .snapshot import create_snapshot
-from locks import free_missing_locks
+from .locks import free_missing_locks
