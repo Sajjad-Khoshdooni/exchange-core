2c5c92f3a87fe33fb8c37aba830be00fcae41647
armin mohammadi <armin.mohamady@gmail.com>
2023-03-11 15:24:32 +0330

    fix authentication class bug

--- accounts/throttle.py
+++ accounts/throttle.py
@@ -2,7 +2,7 @@ import logging
 
 from rest_framework.throttling import UserRateThrottle
 
-from accounts.models import CustomToken
+from accounts.authentication import CustomTokenAuthentication
 
 logger = logging.getLogger(__name__)
 
@@ -10,17 +10,14 @@ logger = logging.getLogger(__name__)
 class CustomUserRateThrottle(UserRateThrottle):
     def allow_request(self, request, view):
         from accounts.views.jwt_views import user_has_delegate_permission
-        used_token_authentication = isinstance(getattr(request, 'successful_authenticator', None), CustomToken)
+        used_token_authentication = isinstance(getattr(request, 'successful_authenticator', None),
+                                               CustomTokenAuthentication)
         if request.auth and request.user and user_has_delegate_permission(request.user) and ((
                 getattr(request.auth, 'token_type', None) == 'access' and
                 hasattr(request.auth, 'payload') and request.auth.payload.get('account_id')
         ) or used_token_authentication):
             return True
         allow_request = super(CustomUserRateThrottle, self).allow_request(request, view)
-        if not allow_request:
-            logger.info(f"throttled {request.auth}, {request.user}, {user_has_delegate_permission(request.user)}, "
-                        f"{getattr(request.auth, 'token_type', None) == 'access'}, {hasattr(request.auth, 'payload')}, "
-                        f"{used_token_authentication}")
         return allow_request
 
 
