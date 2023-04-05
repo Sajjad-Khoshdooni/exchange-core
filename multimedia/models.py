9ca5aa6ee96984e28595ef9bedc1ccf60bd4b69d
Reza <reza.abbasi.sut@gmail.com>
2023-04-05 10:50:18 +0330

    add historical to coin price content

--- multimedia/models.py
+++ multimedia/models.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 
 from django.db import models
+from simple_history.models import HistoricalRecords
 from tinymce.models import HTMLField
 
 
@@ -41,6 +42,8 @@ class Banner(models.Model):
 class CoinPriceContent(models.Model):
     asset = models.OneToOneField(to='ledger.Asset', on_delete=models.PROTECT)
     content = HTMLField()
+
+    history = HistoricalRecords()
 
     def __str__(self):
         return str(self.asset)
