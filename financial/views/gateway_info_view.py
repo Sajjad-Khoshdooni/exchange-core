023597b544f42c962ff2e9b124d0613762025488
Reza <reza.abbasi.sut@gmail.com>
2023-04-26 14:10:56 +0330

    add next_ach_time to gateway

--- financial/views/gateway_info_view.py
+++ financial/views/gateway_info_view.py
@@ -2,12 +2,18 @@ from rest_framework import serializers
 from rest_framework.generics import RetrieveAPIView
 
 from financial.models import Gateway
+from financial.utils.ach import next_ach_clear_time
 
 
 class GatewaySerializer(serializers.ModelSerializer):
+    next_ach_time = serializers.SerializerMethodField
+
+    def get_next_ach_time(self):
+        return next_ach_clear_time()
+
     class Meta:
         model = Gateway
-        fields = ('id', 'min_deposit_amount', 'max_deposit_amount')
+        fields = ('id', 'min_deposit_amount', 'max_deposit_amount', 'next_ach_time')
 
 
 class GatewayInfoView(RetrieveAPIView):
