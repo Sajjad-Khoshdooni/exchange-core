f4d62d0fbf0b61cbf499a230af5734d897b43182
Reza <reza.abbasi.sut@gmail.com>
2023-05-06 17:31:02 +0330

    add deleted to address key

--- ledger/models/deposit_address.py
+++ ledger/models/deposit_address.py
@@ -18,26 +18,26 @@ class DepositAddress(models.Model):
     def get_deposit_address(cls, account, network):
         architecture = request_architecture(network)
 
-        deposit_address = DepositAddress.objects.filter(address_key__account=account, network=network).first()
-        address_key = AddressKey.objects.filter(account=account, architecture=architecture).first()
+        address_key = AddressKey.objects.filter(account=account, architecture=architecture, deleted=False).first()
 
-        if deposit_address:
-            return deposit_address
+        if not address_key:
+            address_dict = AddressRequester().create_wallet(account, architecture)
 
-        elif not address_key:
-            address_dictionary = AddressRequester().create_wallet(account, architecture)
             address_key = AddressKey.objects.create(
                 account=account,
-                address=address_dictionary.get('pointer_address'),
-                public_address=address_dictionary.get('public_address'),
+                address=address_dict.get('pointer_address'),
+                public_address=address_dict.get('public_address'),
                 architecture=architecture
             )
 
-        deposit_address = DepositAddress.objects.create(
-            network=network,
-            address_key=address_key,
-            address=address_key.public_address,
-        )
+        deposit_address = DepositAddress.objects.filter(address_key=address_key, network=network).first()
+
+        if not deposit_address:
+            deposit_address = DepositAddress.objects.create(
+                network=network,
+                address_key=address_key,
+                address=address_key.public_address,
+            )
 
         return deposit_address
 
@@ -48,4 +48,5 @@ class DepositAddress(models.Model):
     class Meta:
         unique_together = (
             ('network', 'address'),
+            ('network', 'address_key'),
         )
