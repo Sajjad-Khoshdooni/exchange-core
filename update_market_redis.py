297b794d729bf4a1da63b75e2fc600bd324b8400
Reza <reza.abbasi.sut@gmail.com>
2023-05-09 11:26:23 +0330

    fix performance

--- update_market_redis.py
+++ update_market_redis.py
@@ -1,11 +1,11 @@
 import logging
 import os
+from collections import defaultdict
 from time import sleep
 
 import msgpack
 from django.core.wsgi import get_wsgi_application
-from django.db.models import F
-
+from django.db.models import F, Sum
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_base.settings")
 application = get_wsgi_application()
@@ -21,21 +21,25 @@ def main():
     logger.info('start market redis updater')
     pipeline = socket_server_redis.pipeline()
     while True:
-        all_open_orders = Order.open_objects.annotate(
-            unfilled_amount=F('amount') - F('filled_amount')
-        ).exclude(unfilled_amount=0).order_by('symbol', 'side').values('symbol', 'side', 'price', 'unfilled_amount')
-        symbols_id_mapping = {symbol.id: symbol for symbol in PairSymbol.objects.all()}
-        for symbol_id in set(map(lambda o: o['symbol'], all_open_orders)):
-            open_orders = list(filter(lambda o: o['symbol'] == symbol_id, all_open_orders))
-            symbol = symbols_id_mapping[symbol_id]
-            open_orders = Order.quantize_values(symbol, open_orders)
-            symbol_name = symbols_id_mapping[symbol_id].name
+        all_open_orders = list(Order.open_objects.values('symbol', 'side', 'price').annotate(
+            unfilled_amount=Sum(F('amount') - F('filled_amount'))
+        ))
+
+        per_symbol_orders = defaultdict(list)
+
+        for order in all_open_orders:
+            per_symbol_orders[order['symbol'], order['side']].append(order)
+
+        for symbol in PairSymbol.objects.filter(enable=True):
+            bids = per_symbol_orders.get((symbol.id, BUY))
+            asks = per_symbol_orders.get((symbol.id, SELL))
+
             depth = {
-                'symbol': symbol_name,
-                'bids': Order.get_formatted_orders(open_orders, symbol, BUY),
-                'asks': Order.get_formatted_orders(open_orders, symbol, SELL),
+                'symbol': symbol.name,
+                'bids': Order.get_formatted_orders(bids, symbol, BUY),
+                'asks': Order.get_formatted_orders(asks, symbol, SELL),
             }
-            pipeline.hset('market_depth_snapshot', symbol_name, msgpack.packb(depth))
+            pipeline.hset('market_depth_snapshot', symbol.name, msgpack.packb(depth))
         pipeline.execute()
         sleep(1)
 
