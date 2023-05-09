0d3c62e6e2e4bb38ebcf07906b9b2f7e56709bb0
Reza <reza.abbasi.sut@gmail.com>
2023-05-09 16:21:08 +0330

    fix bug

--- ledger/tests/test_fast_forward.py
+++ ledger/tests/test_fast_forward.py
@@ -43,7 +43,10 @@ class FastForwardTestCase(TestCase):
         )
         self.receiver_wallet1 = self.asset.get_wallet(account=self.receiver_account1)
 
-    def test_fast_forward1(self):
+    @mock.patch('ledger.models.deposit_address.request_architecture')
+    def test_fast_forward1(self, request_architecture):
+        request_architecture.return_value = 'ETH'
+
         Transfer.check_fast_forward(
             sender_wallet=self.sender_wallet1,
             network=self.network,
@@ -55,7 +58,11 @@ class FastForwardTestCase(TestCase):
             Trx.objects.get(sender=self.sender_wallet1).group_id,
             Transfer.objects.get(deposit_address=self.receiver_deposit_address1).group_id)
 
-    def test_fast_forward2(self):
+    @mock.patch('ledger.models.deposit_address.request_architecture')
+    def test_fast_forward2(self, request_architecture):
+        request_architecture.return_value = 'ETH'
+        print(request_architecture('BSC'))
+
         transfer = Transfer.check_fast_forward(
             sender_wallet=self.sender_wallet1,
             network=self.network,
