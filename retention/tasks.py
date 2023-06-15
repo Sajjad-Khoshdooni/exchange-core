f9efefdc7c19b20d92865b2baa8fffcc2be83b9a
Reza <reza.abbasi.sut@gmail.com>
2023-06-15 18:13:58 +0330

    disable send deposit sms

--- retention/tasks.py
+++ retention/tasks.py
@@ -37,31 +37,3 @@ def send_signup_not_verified_push():
             push_status=Notification.PUSH_WAITING,
             hidden=True,
         )
-
-
-@shared_task()
-def send_signup_not_deposited_sms():
-    if not settings.RETENTION_ENABLE:
-        logger.info('ignored due to retention is off')
-        return
-
-    now = timezone.now()
-    start_time, end_time = now - timedelta(hours=3), now - timedelta(hours=1)
-
-    users = User.objects.filter(
-        first_fiat_deposit_date=None,
-        first_crypto_deposit_date=None,
-        date_joined__range=[start_time, end_time]
-    ).exclude(smsnotification__template=SmsNotification.RECENT_SIGNUP_NOT_DEPOSITED)
-
-    group_id = uuid.uuid5(uuid.NAMESPACE_URL, SmsNotification.RECENT_SIGNUP_NOT_DEPOSITED)
-
-    for user in users:
-        link, _ = Link.objects.get_or_create(user=user, scope=Link.SCOPE_DEPOSIT)
-
-        SmsNotification.objects.get_or_create(
-            recipient=user,
-            group_id=group_id,
-            template=SmsNotification.RECENT_SIGNUP_NOT_DEPOSITED,
-            params={'link': link.get_link()}
-        )
