940392c07e9399d0fd5a141446520277f7464a5c
Armin Mohammadi <armin.mohamady@gmail.com>
2023-07-03 18:20:17 +0330

    minor bug fixed

--- ledger/models/otc_trade.py
+++ ledger/models/otc_trade.py
@@ -241,7 +241,7 @@ class OTCTrade(models.Model):
                         fill_type=Order.MARKET,
                         raise_exception=False
                     )
-                    if order.trades:
+                    if order and order.trades:
                         filled_amount = sum(map(lambda t: t.amount, order.trades))
                         if filled_amount:
                             filled_value = sum(map(lambda t: t.amount * t.price, order.trades))
