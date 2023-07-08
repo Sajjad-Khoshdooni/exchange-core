5630c3a51a4dfe567113942639083e84d4728e56
armin mohammadi <armin.mohamady@gmail.com>
2023-07-08 14:55:13 +0330

    minor bug fixed

--- market/models/order.py
+++ market/models/order.py
@@ -422,7 +422,7 @@ class Order(models.Model):
                     trade_revenues.append(TradeRevenue.new(
                         user_trade=taker_trade if maker_trade.trade_source == Trade.SYSTEM_MAKER else maker_trade, 
                         group_id=taker_trade.group_id,
-                        source=maker_trade.trade_source,
+                        source=TradeRevenue.MAKER if maker_trade.trade_source == Trade.SYSTEM_TAKER else TradeRevenue.TAKER,
                         hedge_key=hedge_key,
                         ignore_trade_value=ignore_trade_value
                     ))
