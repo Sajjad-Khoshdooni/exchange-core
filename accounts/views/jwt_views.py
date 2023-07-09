2ea738b3e4cd267604e5ddf7897407d58cc05baf
Sajad <Sijalkhf@gmail.com>
2023-07-09 09:48:04 +0330

    fix

--- accounts/views/jwt_views.py
+++ accounts/views/jwt_views.py
@@ -122,7 +122,8 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         account = Account.objects.get(user_id=user.pk)
         token['account_id'] = account.id
 
-        refresh_token_model, _ = RefreshTokenModel.objects.get_or_create(token=str('token'))
+        refresh_token = RefreshToken.for_user(user)
+        refresh_token_model, _ = RefreshTokenModel.objects.get_or_create(token=refresh_token)
 
         token['refresh_id'] = refresh_token_model.id
 
