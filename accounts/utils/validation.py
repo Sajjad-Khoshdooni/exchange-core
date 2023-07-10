2455bef1eea5c9b4a0e0c6134caec518872d6eff
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-07-10 09:54:46 +0330

    fix: login activity return

--- accounts/utils/validation.py
+++ accounts/utils/validation.py
@@ -147,7 +147,7 @@ def set_login_activity(request, user, is_sign_up: bool = False, client_info: dic
     ip = get_client_ip(request)
     ip_data = get_ip_data(ip)
 
-    return LoginActivity.objects.get_or_create(
+    login_activity, _ = LoginActivity.objects.get_or_create(
         session=session,
         refresh_token=refresh_token_model,
         defaults={
@@ -161,3 +161,4 @@ def set_login_activity(request, user, is_sign_up: bool = False, client_info: dic
             'native_app': native_app,
         }
     )
+    return login_activity
