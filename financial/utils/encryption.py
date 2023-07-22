609d01bcb7087895d848124022361f1329b4435c
Reza <reza.abbasi.sut@gmail.com>
2023-07-22 18:46:52 +0330

    enable setting encrypted fields from admin

--- financial/utils/encryption.py
+++ financial/utils/encryption.py
@@ -6,10 +6,16 @@ ENCRYPTION_KEY = config('ENCRYPTION_KEY')
 
 
 def encrypt(text: str) -> str:
+    if not text:
+        return text
+
     fernet = Fernet(ENCRYPTION_KEY.encode())
     return fernet.encrypt(text.encode()).decode()
 
 
 def decrypt(encrypted: str) -> str:
+    if not encrypted:
+        return encrypted
+
     fernet = Fernet(ENCRYPTION_KEY.encode())
     return fernet.decrypt(encrypted.encode()).decode()
