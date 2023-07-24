95edff03fb6d0345e90fa840b7a0d2ed3c13e284
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-07-24 15:38:59 +0330

    feat: change password otp & totp

--- accounts/views/change_password_view.py
+++ accounts/views/change_password_view.py
@@ -4,14 +4,16 @@ from django.core.exceptions import ValidationError
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django_otp.plugins.otp_totp.models import TOTPDevice
 
 from accounts.models import VerificationCode
 
 
 class ChangePasswordSerializer(serializers.Serializer):
-    old_password = serializers.CharField(write_only=True)
-    password = serializers.CharField(write_only=True)
-    otp_code = serializers.CharField(write_only=True)
+    old_password = serializers.CharField(required=True, write_only=True)
+    password = serializers.CharField(required=True, write_only=True)
+    otp_code = serializers.CharField(required=True, write_only=True)
+    totp = serializers.CharField(allow_null=True, allow_blank=True, required=False)
 
     def validate(self, data):
 
@@ -20,12 +22,14 @@ class ChangePasswordSerializer(serializers.Serializer):
         password = data.get('password')
         old_pass = data.get('old_password')
         otp_code = VerificationCode.get_by_code(code, user.phone, VerificationCode.SCOPE_CHANGE_PASSWORD, user)
-
+        totp = data.get('totp')
         if not otp_code:
             raise ValidationError({'code': 'کد نامعتبر است.'})
         if not user.check_password(old_pass):
             raise ValidationError({'old_password': 'رمز عبور قبلی بدرستی وارد نشده است'})
-
+        device = TOTPDevice.objects.filter(user=user).first()
+        if not (device is None or not device.confirmed or device.verify_token(totp)):
+            raise ValidationError({'otp': ' رمز موقت نامعتبر است.'})
         validate_password(password=password, user=user)
 
         return data
@@ -41,6 +45,10 @@ class ChangePasswordSerializer(serializers.Serializer):
 
 
 class ChangePasswordView(APIView):
+    def post(self, request):
+        user = request.user
+        VerificationCode.send_otp_code(phone=user.phone, scope=VerificationCode.SCOPE_CHANGE_PASSWORD, user=user)
+        return Response({'msg': 'پیامک با موفقیت ارسال شد.'})
 
     def patch(self, request):
         user = self.request.user
