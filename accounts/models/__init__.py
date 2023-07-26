71bc2a0400f1f12f8d70e9e4a45feafa0272377e
Sajad <Sijalkhf@gmail.com>
2023-07-26 10:40:05 +0330

    remove template

--- accounts/models/__init__.py
+++ accounts/models/__init__.py
@@ -17,4 +17,3 @@ from .sms_notification import SmsNotification
 from .login_activity import LoginActivity
 from .email_notification import EmailNotification
 from .refresh_token import RefreshToken
-from .template import Template
