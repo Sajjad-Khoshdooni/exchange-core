b6635a4ea5e1dc45f2c2b1ce29871129f8c296a0
Reza <reza.abbasi.sut@gmail.com>
2023-07-31 14:17:20 +0330

    add binance_name field

--- ledger/models/coin_category.py
+++ ledger/models/coin_category.py
@@ -6,6 +6,7 @@ from ledger.models import Asset
 class CoinCategory(models.Model):
     name = models.CharField(max_length=32, db_index=True)
     coins = models.ManyToManyField(Asset)
+    binance_name = models.CharField(max_length=32, blank=True)
 
     class Meta:
         verbose_name_plural = verbose_name = 'گروه‌بندی نمایش رمزارزها'
