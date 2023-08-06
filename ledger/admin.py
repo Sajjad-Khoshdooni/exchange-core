7c95c37b3f3cebabebfdb6a9555eb4779f5827f1
Reza <reza.abbasi.sut@gmail.com>
2023-08-06 13:53:27 +0330

    fix minor bug

--- ledger/admin.py
+++ ledger/admin.py
@@ -43,13 +43,14 @@ class AssetAdmin(AdvancedAdmin):
         'symbol', 'enable', 'get_hedge_value', 'get_hedge_value_abs', 'get_hedge_amount', 'get_calc_hedge_amount',
         'get_total_asset', 'get_users_balance', 'get_reserved_amount',
         'order', 'trend', 'trade_enable', 'hedge',
-        'margin_enable', 'publish_date', 'spread_category', 'otc_status', 'price_page', 'distribution_factor'
+        'margin_enable', 'publish_date', 'spread_category', 'otc_status', 'price_page', 'get_distribution_factor'
     )
     list_filter = ('enable', 'trend', 'margin_enable', 'spread_category')
     list_editable = ('enable', 'order', 'trend', 'trade_enable', 'margin_enable', 'hedge', 'price_page')
     search_fields = ('symbol', )
     ordering = ('-enable', '-pin_to_top', '-trend', 'order')
     actions = ('setup_asset', )
+    readonly_fields = ('distribution_factor', )
 
     def save_model(self, request, obj, form, change):
         if Asset.objects.filter(order=obj.order).exclude(id=obj.id).exists():
