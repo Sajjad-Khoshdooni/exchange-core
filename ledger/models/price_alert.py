22f31c54dc0e780b1e1e0ebd6cc12eec5272609e
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-08-07 17:46:06 +0330

    feat: worker demo version

--- ledger/models/price_alert.py
+++ ledger/models/price_alert.py
@@ -6,7 +6,7 @@ from ledger.models import Asset
 
 class PriceTracking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    assets = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = [('user', 'asset')]
--- ledger/tasks/alert.py
+++ ledger/tasks/alert.py
@@ -1,16 +1,31 @@
 from celery import shared_task
+from django.core.cache import cache
 
-from accounts.models import User
+from accounts.models import Notification
 from ledger.models.price_alert import PriceTracking
 from ledger.utils.external_price import get_external_usdt_prices, BUY
 
+
 @shared_task(queue='price_alert')
 def send_price_notifications():
-    PriceTracking.objects.value()
-    coins = list(.values_list('symbol', flat=True))
-    prices = get_external_usdt_prices(
-        coins=coins,
-        side=BUY,
-    )
-
-
+    current_cycle_coins = list(PriceTracking.objects.distinct('asset').values_list('coin__symbol'))
+    current_cycle_prices = get_external_usdt_prices(coins=current_cycle_coins, side=BUY)
+    current_cycle_prices = {coin: value for coin, value in current_cycle_prices.items() if value and value != 0}
+    past_cycle_prices = cache.get('coin_prices')
+    if not past_cycle_prices:
+        cache.set('coin_prices', current_cycle_prices)
+    common_coins = past_cycle_prices.keys() & current_cycle_prices.keys()
+    for coin in common_coins:
+        if abs(current_cycle_prices[coin] / past_cycle_prices[coin] - 1.00) > 0.05:
+            del common_coins[coin]
+    users = PriceTracking.objects.values_list('user', flat=True)
+    for user in users:
+        tracking_coins = list(PriceTracking.objects.filter(user=user).values_list('asset__symbol', flat=True))
+        prices = {coin: current_cycle_prices[coin] for coin in tracking_coins}
+        for price in prices:
+            Notification.send(
+                recipient=user,
+                title='تغییر قیمت',
+                message=f'تغییر قیمت قابل توجه دارد.! {price.coin}'
+            )
+    cache.set('coin_prices', current_cycle_prices)
