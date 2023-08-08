b70836a10478acd7752fc9a4c37dd7a94e53dd6b
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-08-08 15:50:06 +0330

    feat: index added for price trackings

--- ledger/models/price_alert.py
+++ ledger/models/price_alert.py
@@ -9,4 +9,7 @@ class PriceTracking(models.Model):
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
 
     class Meta:
+        indexes = [
+            models.Index(fields=['user', 'asset'], name='price_tracking_idx')
+        ]
         unique_together = [('user', 'asset')]
