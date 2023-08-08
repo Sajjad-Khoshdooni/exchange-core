aa0e0eb82dd96e1fa0844760346443008d1cade3
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-08-08 11:45:03 +0330

    feat: intial price alert view

--- ledger/views/track_coin.py
+++ ledger/views/track_coin.py
@@ -1,7 +1,31 @@
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from ledger.models.price_alert import PriceTracking
+
+
 class AlertViewSerializer(serializers.ModelSerializer):
+    def is_valid(self, raise_exception=True):
+        user = self.context['request'].user
+        data = self.data
+        asset = data['asset']
+        if PriceTracking.objects.filter(user=user, asset=asset).exists():
+            raise ValidationError('بازار انتخاب شده تحت‌نظر می‌باشد.')
+        return data
+
     class Meta:
-        ...
-class AlertView(APIView):
-    ...
+        model = PriceTracking
+        fields = ('asset',)
+
+
+class AlertView(viewsets.ModelViewSet):
+    serializers_class = AlertViewSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, context={'request': request})
+        serializer.is_valid()
+        serializer.perform_create()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
