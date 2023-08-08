b8d485f48b5cb5bdbac8bc2d3b75ff9d11db5756
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-08-08 17:29:22 +0330

    fix: usdt in irt & minor problems

--- ledger/tasks/alert.py
+++ ledger/tasks/alert.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 
 from accounts.models import Notification
 from ledger.models.price_alert import PriceTracking
-from ledger.utils.external_price import get_external_usdt_prices, BUY
+from ledger.utils.external_price import get_external_usdt_prices, USDT, IRT, get_external_price, BUY
 
 
 @shared_task(queue='price_alert')
@@ -12,7 +12,6 @@ def send_price_notifications():
     symbols = list(PriceTracking.objects.distinct('asset').values_list('asset__symbol', flat=True))
     symbols_price = {coin: value for coin, value in
                      get_external_usdt_prices(coins=symbols, side=BUY).items() if value and value != 0}
-
     past_cycle_prices = cache.get('coin_prices')
     if not past_cycle_prices:
         cache.set('coin_prices', symbols_price)
@@ -20,20 +19,20 @@ def send_price_notifications():
 
     common_coins = past_cycle_prices.keys() & symbols_price.keys()
     common_coins_prices = {coin: symbols_price[coin] for coin in common_coins}
-
+    if USDT in common_coins_prices.keys():
+        common_coins_prices[USDT] *= get_external_price(coin=USDT, base_coin=IRT, side=BUY)
     for coin in common_coins:
         if abs(Decimal(symbols_price[coin] / past_cycle_prices[coin]) - Decimal(1.00)) < Decimal(0.05):
             del common_coins_prices[coin]
-
-    users = PriceTracking.objects.distinct('user').select_related('user').all()
-    for user in users:
-        tracking_coins = list(PriceTracking.objects.filter(user=user).values_list('asset__symbol', flat=True))
+    selections = PriceTracking.objects.distinct('user').only('user')
+    for select in selections:
+        tracking_coins = list(PriceTracking.objects.filter(user=select.user).values_list('asset__symbol', flat=True))
         notifying_coins = {coin for coin in tracking_coins if coin in common_coins_prices.keys()}
         for coin in notifying_coins:
             Notification.send(
-                recipient=user,
+                recipient=select.user,
                 title='تغییر قیمت',
-                message=f'تغییر قیمت قابل توجه دارد.!{symbols_price[coin]}'
+                message=f'تغییر قیمت قابل توجه دارد.! {symbols_price[coin]}'
             )
 
     cache.set('coin_prices', symbols_price)
