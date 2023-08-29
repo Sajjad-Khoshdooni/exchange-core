f417d9f47bbd5636ff6dc804a74b522201b7c4c9
armin mohammadi <armin.mohamady@gmail.com>
2023-08-29 17:10:13 +0330

    replace first with all zero index

--- market/models/oco.py
+++ market/models/oco.py
@@ -48,7 +48,7 @@ class OCO(models.Model):
 
     @property
     def filled_amount(self):
-        order = self.order_set.first()
+        order = self.order_set.all()[0]
         if order.filled_amount:
             return order.filled_amount
         if hasattr(self, 'stoploss'):
@@ -76,7 +76,7 @@ class OCO(models.Model):
                 stop_loss.canceled_at = timezone.now()
                 stop_loss.save(update_fields=['canceled_at'])
         elif order_type == self.ORDER:
-            order = self.order_set.first()
+            order = self.order_set.all()[0]
             if order.status == self.NEW:
                 order.status = self.CANCELED
                 order.save(update_fields=['status'])
