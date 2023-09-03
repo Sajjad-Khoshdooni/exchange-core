81d2c7b5c7637d6246ed0a9d665997910506f7d8
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-09-03 16:31:02 +0330

    fix: minor totp fixes

--- accounts/views/forget_password_view.py
+++ accounts/views/forget_password_view.py
@@ -52,11 +52,13 @@ class ForgotPasswordSerializer(serializers.Serializer):
     def create(self, validated_data):
         token = validated_data.pop('token')
         password = validated_data.pop('password')
-        totp = validated_data.pop('totp')
+        totp = validated_data.get('totp', None)
         otp_code = VerificationCode.get_by_token(token, VerificationCode.SCOPE_FORGET_PASSWORD)
+        otp_code.set_token_used()
 
         if not otp_code:
             raise ValidationError({'token': 'توکن نامعتبر است.'})
+
         user = User.objects.get(phone=otp_code.phone)
         if not user.is_2fa_valid(totp):
             raise ValidationError({'otp': ' رمز موقت نامعتبر است.'})
@@ -64,8 +66,6 @@ class ForgotPasswordSerializer(serializers.Serializer):
         validate_password(password=password, user=user)
         user.set_password(password)
         user.save(update_fields=['password'])
-
-        otp_code.set_token_used()
 
         return user
 
