de3e707cdd701f8be773a1fe35c8757bba230051
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-09-03 16:33:25 +0330

    fix: forget password flow

--- accounts/views/forget_password_view.py
+++ accounts/views/forget_password_view.py
@@ -54,10 +54,11 @@ class ForgotPasswordSerializer(serializers.Serializer):
         password = validated_data.pop('password')
         totp = validated_data.get('totp', None)
         otp_code = VerificationCode.get_by_token(token, VerificationCode.SCOPE_FORGET_PASSWORD)
-        otp_code.set_token_used()
 
         if not otp_code:
             raise ValidationError({'token': 'توکن نامعتبر است.'})
+
+        otp_code.set_token_used()
 
         user = User.objects.get(phone=otp_code.phone)
         if not user.is_2fa_valid(totp):
