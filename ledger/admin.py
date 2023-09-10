204b99760861d380be035005060e0a915549c69d
Reza <reza.abbasi.sut@gmail.com>
2023-09-10 10:08:36 +0330

    fix minor

--- ledger/admin.py
+++ ledger/admin.py
@@ -15,20 +15,19 @@ from accounting.models import ReservedAsset
 from accounts.admin_guard import M
 from accounts.admin_guard.admin import AdvancedAdmin
 from accounts.admin_guard.html_tags import anchor_tag
-from accounts.models import Account, User
+from accounts.models import Account
 from accounts.models.user_feature_perm import UserFeaturePerm
 from accounts.utils.admin import url_to_edit_object
 from accounts.utils.validation import gregorian_to_jalali_datetime_str
 from financial.models import Payment
-from ledger.models.asset_alert import AssetAlert, AlertTrigger, BulkAssetAlert
 from ledger import models
-from ledger.models import Asset, Prize, CoinCategory, FastBuyToken, Network, ManualTransaction, BalanceLock, Wallet, \
+from ledger.models import Prize, CoinCategory, FastBuyToken, Network, ManualTransaction, Wallet, \
     ManualTrade, Trx, NetworkAsset
+from ledger.models.asset_alert import AssetAlert, AlertTrigger, BulkAssetAlert
 from ledger.models.wallet import ReserveWallet
 from ledger.utils.external_price import get_external_price, BUY
 from ledger.utils.fields import DONE, PROCESS, PENDING
 from ledger.utils.precision import get_presentation_amount, humanize_presentation, get_symbol_presentation_amount
-from ledger.utils.precision import humanize_number
 from ledger.utils.provider import get_provider_requester
 from ledger.utils.withdraw_verify import RiskFactor
 from market.utils.fix import create_symbols_for_asset
@@ -282,11 +281,11 @@ class OTCTradeAdmin(admin.ModelAdmin):
 
     @admin.display(description='value')
     def get_value(self, otc_trade: models.OTCTrade):
-        return humanize_number(round(otc_trade.otc_request.usdt_value, 2))
+        return humanize_presentation(round(otc_trade.otc_request.usdt_value, 2))
 
     @admin.display(description='value_irt')
     def get_value_irt(self, otc_trade: models.OTCTrade):
-        return humanize_number(round(otc_trade.otc_request.irt_value, 0))
+        return humanize_presentation(round(otc_trade.otc_request.irt_value, 0))
 
     @admin.action(description='Accept Trade')
     def accept_trade(self, request, queryset):
@@ -651,7 +650,7 @@ class FastBuyTokenAdmin(admin.ModelAdmin):
     list_filter = ('status',)
 
     def get_amount(self, fast_buy_token: FastBuyToken):
-        return humanize_number(fast_buy_token.amount)
+        return humanize_presentation(fast_buy_token.amount)
 
     get_amount.short_description = 'مقدار'
 
@@ -687,7 +686,7 @@ class ManualTransactionForm(forms.ModelForm):
 @admin.register(ManualTransaction)
 class ManualTransactionAdmin(admin.ModelAdmin):
     form = ManualTransactionForm
-    list_display = ('created', 'wallet', 'type', 'status', 'amount')
+    list_display = ('created', 'wallet', 'type', 'status', 'get_amount_preview')
     list_filter = ('type', 'status')
     ordering = ('-created',)
     readonly_fields = ('group_id',)
@@ -700,6 +699,10 @@ class ManualTransactionAdmin(admin.ModelAdmin):
             trx.status = PROCESS
             trx.group_id = uuid4()
             trx.save()
+
+    @admin.display(description='amount', ordering='amount')
+    def get_amount_preview(self, mt: ManualTransaction):
+        return humanize_presentation(mt.amount)
 
 
 @admin.register(AssetAlert)
