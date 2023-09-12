3db9a786e77d61c4f8083afafcdcae67dcb77897
Reza <reza.abbasi.sut@gmail.com>
2023-09-12 11:38:24 +0330

    refactor consultation

--- accounts/views/consultation_view.py
+++ accounts/views/consultation_view.py
@@ -1,18 +1,10 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.serializers import ModelSerializer
-from rest_framework.exceptions import ValidationError
 
 from accounts.models import Consultation
 
 
 class ConsultationSerializer(ModelSerializer):
-
-    def validate(self, attrs):
-        user = self.context['request'].user
-        if user.is_consulted:
-            raise ValidationError('کاربر قبلا مشاوره شده است.')
-        attrs['consultee'] = user
-        return attrs
 
     class Meta:
         model = Consultation
@@ -24,3 +16,6 @@ class ConsultationSerializer(ModelSerializer):
 
 class ConsultationView(CreateAPIView):
     serializer_class = ConsultationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(consultee=self.request.user)
