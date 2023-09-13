b18858f3e4d74092e633ddc4c1117fa4f5edc86f
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-09-13 13:06:21 +0330

    feat: withdraw fees added to the system config model

--- accounts/models/system_config.py
+++ accounts/models/system_config.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+from ledger.utils.fields import get_amount_field
+
+from decimal import Decimal
+
 
 class SystemConfig(models.Model):
     active = models.BooleanField()
     is_consultation_available = models.BooleanField(default=False)
+
+    ipg_withdraw_fee_min = models.SmallIntegerField(default=1000)
+    ipg_withdraw_fee_max = models.SmallIntegerField(default=5000)
+    ipg_withdraw_fee_percent = get_amount_field(default=Decimal('0.05'))
 
     @classmethod
     def get_system_config(cls) -> 'SystemConfig':
