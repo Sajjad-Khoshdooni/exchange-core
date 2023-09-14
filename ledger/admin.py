2e6fb92572e38b2a71930b066bb0217de93553d1
Reza <reza.abbasi.sut@gmail.com>
2023-09-14 15:56:08 +0330

    fix minor

--- ledger/admin.py
+++ ledger/admin.py
@@ -553,6 +553,7 @@ class CloseRequestAdmin(admin.ModelAdmin):
 class AddressBookAdmin(admin.ModelAdmin):
     list_display = ('name', 'account', 'network', 'address', 'asset',)
     search_fields = ('address', 'name')
+    readonly_fields = ('account', )
 
 
 class PrizeUserFilter(admin.SimpleListFilter):
