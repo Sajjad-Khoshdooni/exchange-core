821cf65171266f5f92ac2bf670ff4a46184a39de
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-09-18 14:59:25 +0330

    feat: base section defined

--- multimedia/models/section.py
+++ multimedia/models/section.py
@@ -1,18 +1,24 @@
 from django.db import models
 from django.utils.text import slugify
 
+import uuid
 
 from multimedia.models import Image
 
 
 class Section(models.Model):
     parent_section = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
+
+    icon = Image()
     title = models.CharField(max_length=30)
     title_en = models.CharField(max_length=30)
-    icon = Image()
+
     description = models.TextField(blank=True)
+
+    id = models.UUIDField(default=uuid.uuid4(), editable=False)
     slug = models.SlugField(max_length=255)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
+        self.slug = slugify(self.title_en)
         super().save(*args, **kwargs)
+
