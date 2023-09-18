8704a14b0763e181b6aa6b1b9f90fe4956e13294
Reza <reza.abbasi.sut@gmail.com>
2023-09-18 17:41:08 +0330

    fix minor

--- ledger/views/coin_category_list_view.py
+++ ledger/views/coin_category_list_view.py
@@ -5,6 +5,11 @@ from ledger.models import CoinCategory
 
 
 class CoinCategorySerializer(serializers.ModelSerializer):
+    header = serializers.SerializerMethodField()
+
+    def get_header(self, category: CoinCategory):
+        return category.header or 'قیمت لحظه‌ای ارز‌های دیجیتال'
+
     class Meta:
         model = CoinCategory
         fields = ('name', 'title', 'description', 'header')
