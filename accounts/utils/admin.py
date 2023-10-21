528fe72428f8dfc40e73b1867abd1734874c13fd
Reza <reza.abbasi.sut@gmail.com>
2023-10-21 11:59:11 +0330

    fix alerting

--- accounts/utils/admin.py
+++ accounts/utils/admin.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 from django.conf import settings
 from django.urls import reverse
 
@@ -6,5 +8,10 @@ def url_to_edit_object(obj):
     return settings.HOST_URL + reverse('admin:%s_%s_change' % (obj._meta.app_label,  obj._meta.model_name),  args=[obj.id] )
 
 
-def url_to_admin_list(obj):
-    return settings.HOST_URL + reverse('admin:%s_%s_changelist' % (obj._meta.app_label,  obj._meta.model_name))
+def url_to_admin_list(obj, filters: dict = None):
+    url = settings.HOST_URL + reverse('admin:%s_%s_changelist' % (obj._meta.app_label,  obj._meta.model_name))
+
+    if filters:
+        url += '?' + urlencode(filters)
+
+    return url
