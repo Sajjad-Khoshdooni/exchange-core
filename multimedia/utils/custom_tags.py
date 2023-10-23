a9de7a3eb8bcf88485e82b1d8a204cc3a450e7a7
Reza <reza.abbasi.sut@gmail.com>
2023-10-23 15:55:30 +0330

    fix minor

--- multimedia/utils/custom_tags.py
+++ multimedia/utils/custom_tags.py
@@ -6,8 +6,12 @@ from bs4 import BeautifulSoup
 VIDEO_REGEX = re.compile(r'VIDEO \s*([^ ]+)\s*([^ ]+)')
 REPLACE_WITH = '<video style="width: 100%" controls poster="{1}"><source src="{0}" type="video/mp4" />مرورگر شما قابلیت نمایش ویدیو را ندارد.</video>'
 
+EMPTY_STYLE_REGEX = re.compile(r' style=\"\s*\"')
+
 
 def post_render_html(html: str) -> str:
+    html = html.replace('color: rgb(0, 0, 0);', '').replace('background-color: transparent;', '')
+    html = EMPTY_STYLE_REGEX.sub("", html)
     tree = BeautifulSoup(html, 'html.parser')
     for node in tree.findAll('pre'):
         rgx = VIDEO_REGEX.match(node.text.strip())
