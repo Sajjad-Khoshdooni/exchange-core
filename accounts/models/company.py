077a1374bd0d3b83c7e5f0fe7052cc22edf9425d
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-10-25 12:38:17 +0330

    style: company verbose name single & plural

--- accounts/models/company.py
+++ accounts/models/company.py
@@ -5,3 +5,7 @@ from accounts.validators import company_national_id_validator
 
 class Company(models.Model):
     national_id = models.CharField(validators=[company_national_id_validator], unique=True)
+
+    class Meta:
+        verbose_name = 'شرکت'
+        verbose_name_plural = 'شرکت‌ها'
