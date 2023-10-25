e0b53b231c63f5aa06f1f9ca58caf573e4036d6e
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-10-25 17:36:35 +0330

    feat: file upload api has been added

--- multimedia/urls.py
+++ multimedia/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from multimedia.views import ImageCreateView, BannerListView, SectionsView, ArticleView, ArticleSearchView, \
-    PinnedArticlesView
+    PinnedArticlesView, FileCreateView
 
 urlpatterns = [
     path('image/', ImageCreateView.as_view()),
+    path('file/', FileCreateView.as_view()),
     path('banners/', BannerListView.as_view()),
     path('faq/sections/', SectionsView.as_view()),
     path('faq/articles/<str:slug>/', ArticleView.as_view()),
