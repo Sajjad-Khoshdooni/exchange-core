ffd3a1f686712726a5dad956967af7d2cb5bf31f
Reza <reza.abbasi.sut@gmail.com>
2023-10-30 19:56:53 +0330

    fix minor

--- update_market_redis_liveness.py
+++ update_market_redis_liveness.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     socket_server_redis = Redis.from_url(config('SOCKET_SERVER_CACHE_LOCATION',  default='redis://127.0.0.1:6379'), decode_responses=True)
 
     if not socket_server_redis.get('market_depth_snapshot_liveness'):
-        raise Exception('Liveness failed')
-
-    print("Ok!")
+        print("Error...")
+    else:
+        print("OK!")
