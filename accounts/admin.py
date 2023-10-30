27b0bb8df69647db706b6ee8e52e04ae9ea1ee24
Saman Hazemi Jebelli <samanhazemijebelli@gmail.com>
2023-10-30 18:40:38 +0330

    fix: user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -309,7 +309,7 @@ class CustomUserAdmin(ModelAdminJalaliMixin, SimpleHistoryAdmin, AdvancedAdmin, 
             'get_revenue_of_referral', 'get_referred_count', 'get_revenue_of_referred'
         )}),
         (_('اطلاعات اضافی'), {'fields': (
-            'is_price_notif_on', 'is_suspended', 'suspended_until', 'suspension_reason', 'is_consulted', 'is_2fa_active'
+            'is_price_notif_on', 'is_suspended', 'suspended_until', 'suspension_reason', 'is_in_process', 'is_2fa_active'
         )})
     )
 
@@ -339,7 +339,7 @@ class CustomUserAdmin(ModelAdminJalaliMixin, SimpleHistoryAdmin, AdvancedAdmin, 
         'get_fill_order_address', 'selfie_image_verifier', 'get_revenue_of_referral', 'get_referred_count',
         'get_revenue_of_referred', 'get_open_order_address', 'get_selfie_image_uploaded', 'get_referred_user',
         'get_login_activity_link', 'get_last_trade', 'get_total_balance_irt_admin', 'get_order_link',
-        'get_notifications_link', 'get_staking_link', 'get_prizes_link', 'is_suspended', 'is_consulted',
+        'get_notifications_link', 'get_staking_link', 'get_prizes_link', 'is_suspended', 'is_in_process',
         'suspension_reason', 'get_bots_link', 'is_2fa_active'
     )
     preserve_filters = ('archived', )
