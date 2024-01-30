480cf7306ef0a2f74ebbee62540acf936d0a2f43
Reza <reza.abbasi.sut@gmail.com>
2024-01-30 17:55:18 +0330

    fix url

--- financial/urls.py
+++ financial/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     path('withdraw/request/', WithdrawRequestView.as_view({
         'post': 'create',
     })),
-    path('withdraw/request/<int:pk>', WithdrawRequestView.as_view({
+    path('withdraw/request/<int:pk>/', WithdrawRequestView.as_view({
         'delete': 'destroy'
     })),
     path('withdraw/list/', WithdrawHistoryView.as_view()),
